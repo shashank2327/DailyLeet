dfbd7d139a89c250722c0452212aa86087480ce8
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-01-07 19:09:24 +0530

    Time: 9 ms (6.83%) | Memory: 44.8 MB (6.00%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -7,7 +7,7 @@ class Solution {
 
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (i != j && words[j].contains(words[i])) {
+                if (i != j && words[j].length() >= words[i].length() && strStr(words[j], words[i])) {
                     ans.add(words[i]);
                     break;
                 }
@@ -15,5 +15,49 @@ class Solution {
         }
 
         return ans;
+    }
+
+    public boolean strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return true;
+        }
+
+            // Compute the LPS array
+        int[] lps = new int[needle.length()];
+        int prevLPS = 0, i = 1;
+        while (i < needle.length()) {
+            if (needle.charAt(i) == needle.charAt(prevLPS)) {
+                lps[i] = prevLPS + 1;
+                prevLPS++;
+                i++;
+            } else {
+                if (prevLPS == 0) {
+                    lps[i] = 0;
+                    i++;
+                } else {
+                    prevLPS = lps[prevLPS - 1];
+                }
+            }
+        }
+
+            // Search for the needle in the haystack
+        int j = 0; // Pointer for needle
+        i = 0; // Pointer for haystack
+        while (i < haystack.length()) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                if (j == 0) {
+                    i++;
+                } else {
+                    j = lps[j - 1];
+                }
+            }
+            if (j == needle.length()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
