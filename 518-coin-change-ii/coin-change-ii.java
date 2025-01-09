a248b16f52b3c9c675308e8b4c26633867f35776
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-01-09 23:53:15 +0530

    Time: 9 ms (63.66%) | Memory: 44.7 MB (68.37%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,21 +2,22 @@ class Solution {
     
     public int change(int x, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n + 1][x + 1];
-        Arrays.sort(coins);
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
+        int[] nextState = new int[x + 1];
+        nextState[0] = 1;
+
         for (int i = n - 1; i >= 0; i--) {
+            int[] currentState = new int[x + 1];
+            currentState[0] = 1;
             for (int j = 1; j <= x; j++) {
-                int skip = dp[i + 1][j];
+                int skip = nextState[j];
                 int pick = 0;
                 if (coins[i] <= j) {
-                    pick = dp[i][j - coins[i]];
+                    pick = currentState[j - coins[i]];
                 }
-                dp[i][j] = (skip + pick);
+                currentState[j] = (skip + pick);
             }
+            nextState = currentState;
         }
-        return dp[0][x];
+        return nextState[x];
     }
 }
