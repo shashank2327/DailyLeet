b9b2e260d77b3b98f68780145c4bd08e56053644
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-01-18 19:18:12 +0530

    Time: 2 ms (69.26%) | Memory: 42.2 MB (7.20%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,29 +1,22 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
+
         int n = citations.length;
-        int max = 0;
-        for (int i = 1; i <= n; i++) {
-            int up = upperBound(citations, i);
-            int actual = up + 1;
-            int numberOfPapers = n - actual;
-            if (numberOfPapers >= i) {
-                max = i;
-            }
+        int low = 0;
+        int high = n - 1;
+
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+
+            if(citations[mid] == n - mid )
+                return citations[mid];
+            else if(citations[mid] > n - mid)
+                high = mid -1;
+            else
+                low = mid + 1;
         }
-        return max;
-    }
-    private int upperBound(int[] a, int k) {
-        int l = 0;
-        int r = a.length - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (a[mid] >= k) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return r;
+
+        return n - low;
     }
 }
