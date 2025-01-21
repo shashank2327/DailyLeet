915d95fd777acf2b3bc078fa1a4aff25369c3d30
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-01-21 19:46:58 +0530

    Time: 5 ms (58.03%) | Memory: 57.7 MB (48.19%) - LeetSync

--- 2145-grid-game/grid-game.java
+++ 2145-grid-game/grid-game.java
@@ -1,18 +1,23 @@
 class Solution {
     public long gridGame(int[][] grid) {
+
         long n = grid[0].length;
+
+
         long[] rowPrefix = new long[(int)n];
         rowPrefix[0] = grid[0][0];
         for (int i = 1; i < n; i++) {
             rowPrefix[i] = rowPrefix[i - 1] + grid[0][i];
         }
+
+
         long[] colPrefix = new long[(int)n];
         colPrefix[0] = grid[1][0];
         for (int i = 1; i < n; i++) {
             colPrefix[i] = colPrefix[i - 1] + grid[1][i];
         }
 
-        long[] paths = new long[(int)n];
+        long min = Long.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
             long left = 0;
@@ -23,10 +28,9 @@ class Solution {
             if (i < n - 1) {
                 right = rowPrefix[(int)(n - 1)] - rowPrefix[i];
             }
-            paths[i] = Math.max(left, right);
+            min = Math.min(min, Math.max(left, right));
         }
 
-        Arrays.sort(paths);
-        return paths[0];
+        return min;
     }
 }
