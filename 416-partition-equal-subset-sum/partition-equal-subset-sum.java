e36b69576ae32fc30111c07ff1206fa8a91a37f0
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-01-27 22:17:53 +0530

    Time: 28 ms (76.19%) | Memory: 42.2 MB (88.74%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -13,26 +13,43 @@ class Solution {
     private boolean subsetSum(int[] a, int target) {
         int n = a.length;
         
-        boolean[][] dp = new boolean[n][target + 1];
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
-        }
-        Arrays.sort(a);
-        if (a[0] > target) return false;
-        dp[0][a[0]] = true;
+        // 2D - Array
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= target; j++) {
-                boolean notTaken = dp[i- 1][j];
-                boolean taken = false;
-                if (a[i] <= j) {
-                    taken = dp[i - 1][j - a[i]];
-                }
+        // boolean[][] dp = new boolean[n][target + 1];
+        // for (int i = 0; i < n; i++) {
+        //     dp[i][0] = true;
+        // }
+        // Arrays.sort(a);
+        // if (a[0] > target) return false;
+        // dp[0][a[0]] = true;
 
-                dp[i][j] = taken || notTaken;
+        // for (int i = 1; i < n; i++) {
+        //     for (int j = 0; j <= target; j++) {
+        //         boolean notTaken = dp[i- 1][j];
+        //         boolean taken = false;
+        //         if (a[i] <= j) {
+        //             taken = dp[i - 1][j - a[i]];
+        //         }
+
+        //         dp[i][j] = taken || notTaken;
+        //     }
+        // }
+
+        // return dp[n - 1][target];
+
+
+
+        // Space Optimisation 1D
+
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true; // a suset with sum 0 is always true;
+
+        for (int num : a) {
+            for (int j = target; j >= num; j--) {
+                // to achieve sum j we either dont include the curr num or include the current if (it target was achieveable with the current num dp[j - num] was true)
+                dp[j] = dp[j] || dp[j - num];
             }
         }
-
-        return dp[n - 1][target];
+        return dp[target];
     }
 }
