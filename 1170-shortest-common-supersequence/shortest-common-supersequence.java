ce4730f9dc0bcfd37aaed303637639d0f4877b5c
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-02-01 11:21:07 +0530

    Time: 21 ms (87.05%) | Memory: 50.3 MB (68.09%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -19,41 +19,34 @@ class Solution {
                 }
             }
         }
-
-        int len = dp[n1][n2];
         int i = n1;
         int j = n2;
-
-        int index = len - 1;
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
 
         while (i > 0 && j > 0) {
             if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                ans += text1.charAt(i - 1);
-                index--;
+                ans.append(text1.charAt(i - 1));
                 i--;
                 j--;
             } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                ans += text1.charAt(i - 1);
+                ans.append(text1.charAt(i - 1));
                 i--;
             } else {
-                ans += text2.charAt(j - 1);
+                ans.append(text2.charAt(j - 1));
                 j--;
             }
         }
 
 
         while (i > 0) {
-            ans += text1.charAt(i - 1);
+            ans.append(text1.charAt(i - 1));
             i--;
         }
 
         while (j > 0) {
-            ans += text2.charAt(j - 1);
+            ans.append(text2.charAt(j - 1));
             j--;
         }
-
-        String ans2=new StringBuilder(ans).reverse().toString();
-        return ans2;
+        return ans.reverse().toString();
     }
 }
