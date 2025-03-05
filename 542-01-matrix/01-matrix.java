971eaad0cec538640ab69c5e4f441ca0203fcc04
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-03-05 23:08:39 +0530

    Time: 16 ms (26.93%) | Memory: 47.4 MB (54.45%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,52 @@
+class Node {
+    int first;
+    int second;
+    int third;
+    Node (int first, int second, int third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
+
+
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int[][] vis = new int[n][m];
+        int[][] dis = new int[n][m];
+        Queue<Node> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    q.add(new Node(i, j, 0));
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        while (!q.isEmpty()) {
+            int row = q.peek().first;
+            int col = q.peek().second;
+            int steps = q.peek().third;
+            q.remove();
+            dis[row][col] = steps;
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + dx[i];
+                int ncol = col + dy[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0) {
+                    vis[nrow][ncol] = 1;
+                    q.add(new Node(nrow, ncol, steps + 1));
+                }
+            }
+        }
+
+        return dis;
+    }
+}
