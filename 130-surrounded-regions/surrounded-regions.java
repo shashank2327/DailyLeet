d8772960ff4d383305f9f5db6385b238c6e35fe3
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-03-06 21:59:55 +0530

    Time: 2 ms (86.10%) | Memory: 45.2 MB (92.44%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,60 @@
+class Solution {
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int[][] vis = new int[n][m];
+
+        // traversing first row ans last row;
+        for (int j = 0; j < m; j++) {
+
+            if (vis[0][j] == 0 && board[0][j] == 'O') {
+                dfs(0, j, vis, board, dx, dy);
+            }
+
+            if (vis[n - 1][j] == 0 && board[n - 1][j] == 'O') {
+                dfs(n - 1, j, vis, board, dx, dy);
+            }
+
+        }
+
+        // traversing first col and last col
+        for (int j = 0; j < n; j++) {
+
+            if (vis[j][0] == 0 && board[j][0] == 'O') {
+                dfs(j, 0, vis, board, dx, dy);
+            }
+
+            if (vis[j][m - 1] == 0 && board[j][m - 1] == 'O') {
+                dfs(j, m - 1, vis, board, dx, dy);
+            }
+
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] == 0 && board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    public void dfs(int row, int col, int[][] vis, char[][] mat, int[] dx, int[] dy) {
+        vis[row][col] = 1;
+        int n = mat.length;
+        int m = mat[0].length;
+
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + dx[i];
+            int ncol = col + dy[i];
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 
+            && mat[nrow][ncol] == 'O') {
+                dfs(nrow, ncol, vis, mat, dx, dy);
+            }
+        }
+    }
+}
