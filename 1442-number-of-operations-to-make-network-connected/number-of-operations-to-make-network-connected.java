fdbe6af8e940644bca6e18ee24af26cee56d9167
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-03-19 19:06:23 +0530

    Time: 20 ms (19.07%) | Memory: 69.1 MB (22.04%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -0,0 +1,91 @@
+public class DisjointSet {
+
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+
+    // Constructor (Taking number of nodes);
+    public DisjointSet(int n) {
+        for (int i = 0; i <+ n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+
+    // Path compression
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+
+    // Finding union using rank;
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_u) > rank.get(ulp_v)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+
+
+    // Finding union using size;
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_u) + size.get(ulp_v));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+
+
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        DisjointSet ds = new DisjointSet(n);
+        int extra = 0;
+        int m = connections.length;
+        for (int i = 0; i < m; i++) {
+            int u = connections[i][0];
+            int v = connections[i][1];
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                extra++;
+            } else {
+                ds.unionBySize(u, v);
+            }
+        }
+
+        int cntC = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.parent.get(i) == i) cntC++;
+        }
+
+        int ans = cntC - 1;
+
+        if (extra >= ans) return ans;
+        return -1;
+    }
+}
