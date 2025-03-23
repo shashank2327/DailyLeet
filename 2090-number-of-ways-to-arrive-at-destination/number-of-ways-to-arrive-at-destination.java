1d022004f8ce1c934520f7eedee93888ebce2dfe
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-03-23 08:48:14 +0530

    Time: 12 ms (78.21%) | Memory: 49.5 MB (46.00%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int countPaths(int n, int[][] roads) {
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+
+        for (int[] x : roads) {
+            adj.get(x[0]).add(new int[]{x[2], x[1]});
+            adj.get(x[1]).add(new int[]{x[2], x[0]});
+        }
+
+        int mod = 1000000007;
+        PriorityQueue<long[]> q = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));  // Use long
+        q.add(new long[]{0, 0});
+        long[] time = new long[n];  // Use long for distances
+        Arrays.fill(time, Long.MAX_VALUE);
+        int[] count = new int[n];
+        time[0] = 0;
+        count[0] = 1;
+
+        while (!q.isEmpty()) {
+            long[] curr = q.poll();
+            int node = (int) curr[1];
+            long currTime = curr[0];
+
+            if (time[node] < currTime) continue;
+
+            for (int[] x : adj.get(node)) {
+                long nextTime = currTime + x[0];  // Use long
+
+                if (nextTime < time[x[1]]) {
+                    time[x[1]] = nextTime;
+                    count[x[1]] = count[node];  // Reset count because a new shortest path is found
+                    q.offer(new long[]{time[x[1]], x[1]});
+                } else if (nextTime == time[x[1]]) {
+                    count[x[1]] = (count[x[1]] + count[node]) % mod;
+                }
+            }
+        }
+
+        return count[n - 1];
+    }
+}
