a94e0f805852e954d3960d94f160653babde6467
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-03 18:04:52 +0530

    Time: 6 ms (74.73%) | Memory: 46.2 MB (13.37%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,34 +1,40 @@
 class Solution {
-    public boolean canFinish(int V, int[][] prerequisites) {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++) {
+        for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
 
-        int m = prerequisites.length;
-        for (int i = 0; i < m; i++) {
-            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        int n = prerequisites.length;
+
+        for (int i = 0; i < n; i++) {
+            int b = prerequisites[i][1];
+            int a = prerequisites[i][0];
+            adj.get(b).add(a);
         }
 
-        int[] indegree = new int[V];
-        for (int i = 0; i < V; i++) {
+
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
             for (int it : adj.get(i)) {
                 indegree[it]++;
             }
         }
 
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < V; i++) {
+
+        for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
                 q.add(i);
             }
         }
 
         int cnt = 0;
-        int i = 0;
-        while(!q.isEmpty()) {
-            int node = q.peek();
-            q.remove();
+        
+        while (!q.isEmpty()) {
+            int node = q.poll();
             cnt++;
 
             for (int it : adj.get(node)) {
@@ -39,6 +45,10 @@ class Solution {
             }
         }
 
-        return cnt == V;
+        return cnt == numCourses;
     }
 }
+
+// [a, b]
+// b ----> a // b should come first then a should come.
+// I will not be able to take course if there is cycle present.
