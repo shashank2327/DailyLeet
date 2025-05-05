d8360633178a1ed4bec83a767f72cecddfe31557
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-05 17:32:32 +0530

    Time: 11 ms (89.57%) | Memory: 49.1 MB (79.29%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,38 +1,39 @@
-import java.util.*;
-
 class Solution {
     public int countPaths(int n, int[][] roads) {
         ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
+        }
 
         for (int[] x : roads) {
             adj.get(x[0]).add(new int[]{x[2], x[1]});
             adj.get(x[1]).add(new int[]{x[2], x[0]});
         }
 
-        int mod = 1000000007;
-        PriorityQueue<long[]> q = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));  // Use long
+        int mod = 1_000_000_007;
+        PriorityQueue<long[]> q = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));
         q.add(new long[]{0, 0});
-        long[] time = new long[n];  // Use long for distances
+        long[] time = new long[n];
         Arrays.fill(time, Long.MAX_VALUE);
         int[] count = new int[n];
+
         time[0] = 0;
         count[0] = 1;
 
         while (!q.isEmpty()) {
             long[] curr = q.poll();
+
             int node = (int) curr[1];
             long currTime = curr[0];
 
             if (time[node] < currTime) continue;
 
             for (int[] x : adj.get(node)) {
-                long nextTime = currTime + x[0];  // Use long
+                long nextTime = (currTime + x[0]);
 
                 if (nextTime < time[x[1]]) {
                     time[x[1]] = nextTime;
-                    count[x[1]] = count[node];  // Reset count because a new shortest path is found
+                    count[x[1]] = count[node];
                     q.offer(new long[]{time[x[1]], x[1]});
                 } else if (nextTime == time[x[1]]) {
                     count[x[1]] = (count[x[1]] + count[node]) % mod;
