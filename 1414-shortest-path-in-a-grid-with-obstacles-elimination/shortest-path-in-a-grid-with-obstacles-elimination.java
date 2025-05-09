30c456277bfb1baa0e9d91a16f3b01a9eeaf4f4e
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-09 09:41:46 +0530

    Time: 24 ms (55.66%) | Memory: 46.3 MB (30.31%) - LeetSync

---
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -0,0 +1,55 @@
+// class Info {
+//     int dist;
+//     int stp;
+//     int row;
+//     int col;
+
+//     public Info (int dist, int stp, int row, int col) {
+//         this.dist = dist;
+//         this.stp = stp;
+//         this.row = row;
+//         this.col = col;
+//     }
+// }
+
+class Solution {
+    public int shortestPath(int[][] grid, int k) {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][][] vis = new boolean[n][m][k + 1];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {0, 0, k});
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        int steps = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size != 0) {
+                int[] curr = q.peek();
+                q.remove();
+                if (curr[0] == n - 1 && curr[1] == m - 1) return steps;
+
+                for (int i = 0; i < 4; i++) {
+                    int nr = curr[0] + dx[i];
+                    int nc = curr[1] + dy[i];
+                    int obs = curr[2];
+
+                    if (nr >= 0 && nc >= 0 && nr < n && nc < m) {
+                        if (grid[nr][nc] == 0 && !vis[nr][nc][obs]) {
+                            vis[nr][nc][obs] = true;
+                            q.add(new int[]{nr, nc, obs});
+                        } else if (grid[nr][nc] == 1 && !vis[nr][nc][obs] && obs > 0) {
+                            vis[nr][nc][obs] = true;
+                            q.add(new int[]{nr , nc, obs - 1});
+                        }
+                    }
+                }
+                size--;
+            }
+            steps++;
+        }
+        return -1;
+    }
+}
+
+// Take out every Time shortest distance from queue but if the no. of removal is greater than k then continue
