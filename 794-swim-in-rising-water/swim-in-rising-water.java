e4f9683b8c015b52dad34eab95ac788614a87460
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-09 22:01:30 +0530

    Time: 11 ms (60.82%) | Memory: 45.2 MB (11.05%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -0,0 +1,58 @@
+class Tuple {
+    int time;
+    int row;
+    int col;
+
+    public Tuple(int time, int row, int col) {
+        this.time = time;
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int n = grid.length;
+        
+        int[][] tm = new int[n][n];
+        for (int[] x : tm) {
+            Arrays.fill(x, (int)1e9);
+        }
+        tm[0][0] = grid[0][0];
+
+        Queue<Tuple> q = new LinkedList<>();
+        q.add(new Tuple(grid[0][0], 0, 0));
+
+        while (!q.isEmpty()) {
+            Tuple it = q.poll();
+            int t = it.time;
+            int r = it.row;
+            int c = it.col;
+
+            if (t > tm[r][c]) continue;
+
+            int[] dx = {0, 0, 1, -1};
+            int[] dy = {1, -1, 0, 0};
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n) {
+                    if (grid[nr][nc] <= t) {
+                        if (t < tm[nr][nc]) {
+                            tm[nr][nc] = t;
+                            q.add(new Tuple(t, nr, nc));
+                        }
+                    } else {
+                        if (grid[nr][nc] < tm[nr][nc]) {
+                            tm[nr][nc] = grid[nr][nc];
+                            q.add(new Tuple(grid[nr][nc], nr, nc));
+                        }
+                    }
+                }
+            }
+        }
+
+        return tm[n - 1][n - 1];
+    }
+}
