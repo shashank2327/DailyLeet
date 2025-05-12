7b466aa380884782aab9180b6908e31fba07af86
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-12 09:33:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (5.20%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return memoFunction(n, memo);
+    }
+
+    // Every time I have two options
+    // Either to jump one step or to jump 2 step;
+    // if numbers of steps i completed through jumping is > n then return 0 if = then retur 1;
+
+    private int recursiveFunction(int n) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+
+        int ways = recursiveFunction(n - 1) + recursiveFunction(n - 2);
+        return ways;
+    }
+
+
+    private int memoFunction(int n, int[] memo) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+        if (memo[n] != -1) return memo[n];
+        int ways = memoFunction(n - 1, memo) + memoFunction(n - 2, memo);
+        memo[n] = ways;
+        return memo[n];
+    }
+}
