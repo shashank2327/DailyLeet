1785778226e2a20f0108404631ceb9d8313a0680
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-12 10:57:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (49.54%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,43 +1,16 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int[] memo = new int[cost.length];
-        Arrays.fill(memo, - 1);
-        return memoSol(-1, cost, memo);
+        int n = cost.length;
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < n; i++) {
+            int way1 = dp[i - 1] + cost[i];
+            int way2 = dp[i - 2] + cost[i];
+            dp[i] = Math.min(way1, way2);
+        }
+
+        return Math.min(dp[n - 1], dp[n - 2]);
     }
-    private int minRecur(int index, int[] cost) {
-        if (index >= cost.length - 1) return 0;
-
-        int oneJump = 0;
-        int twoJump = 0;
-
-        if (index + 1 <= cost.length - 1) {
-            oneJump = cost[index + 1] + minRecur(index + 1, cost);
-        }
-        if (index + 2 <= cost.length - 1) {
-            twoJump = cost[index + 2] + minRecur(index + 2, cost);
-        }
-        
-
-        return Math.min(oneJump, twoJump);
-
-    }
-
-    private int memoSol(int index, int[] cost, int[] memo) {
-        if (index >= cost.length - 1) return 0;
-        if (index >= 0 && memo[index] != -1) return memo[index];
-        int oneJump = 0;
-        int twoJump = 0;
-
-        if (index + 1 <= cost.length - 1) {
-            oneJump = cost[index + 1] + memoSol(index + 1, cost, memo);
-        }
-        if (index + 2 <= cost.length - 1) {
-            twoJump = cost[index + 2] + memoSol(index + 2, cost, memo);
-        }
-        
-        if (index >= 0) {
-            memo[index] = Math.min(oneJump, twoJump);
-        }
-        return  Math.min(oneJump, twoJump);
-    }
+    
 }
