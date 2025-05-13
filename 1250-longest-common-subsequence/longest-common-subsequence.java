02678bbdda21bed6ada69d2078453a940b26e808
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-13 22:20:53 +0530

    Time: 22 ms (45.63%) | Memory: 55.8 MB (5.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,12 +2,27 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
+
+ /*-----------------------------------------------------------------------------------------------------------*/       
         int[][] memo = new int[n][m];
         for (int[] x : memo) Arrays.fill(x, -1);
-        return f(text1, text2, n - 1, m - 1, memo);
+        // return f(text1, text2, n - 1, m - 1, memo);
+/*------------------------------------------------------------------------------------------------------------*/
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 
-    // Recursive
+    // Recursive + memo
     private int f(String s1, String s2, int i, int j, int[][] memo) {
         if (i < 0 || j < 0) return 0;
         if (memo[i][j] != -1) return memo[i][j];
