b435797f91b78b2f7c71ba0e0a3240ceaa7a0ef5
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-13 23:02:26 +0530

    Time: 20 ms (79.91%) | Memory: 45.4 MB (78.80%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        int x = longestPalindromeSubseq(s);
+        return n - x;
+    }
+
+/*------------------------code for longest palindromic subseq-----------------------------------------*/
+
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] memo = new int[n][n];
+        for (int[] x : memo) Arrays.fill(x, -1);
+        return f(s, 0, n - 1, memo);
+
+    }
+
+    private int f(String s, int i, int j,int[][] memo) {
+        if (i == j) {
+            return 1;
+        }
+        if (i > j) {
+            return 0;
+        }
+        if (memo[i][j] != -1) return memo[i][j];
+        if (s.charAt(i) == s.charAt(j)) {
+            memo[i][j] = 2 + f(s, i + 1, j - 1, memo);
+            return memo[i][j];
+        }
+        memo[i][j] =  Math.max(f(s, i + 1, j, memo), f(s, i, j - 1, memo));
+        return memo[i][j];
+    }
+}
