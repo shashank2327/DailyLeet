17e7858b0e0103ae05253a788414bbc26ca2736f
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-14 21:45:34 +0530

    Time: 1 ms (81.39%) | Memory: 41 MB (99.96%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -5,13 +5,16 @@ class Solution {
         for (int[] x : memo) {
             Arrays.fill(x, Integer.MIN_VALUE);
         }
-        return f(prices, 0, 1, memo);
+        // return f(prices, 0, 1, memo);
+
+        return stockProfit(prices);
 
     }
 
 
     // bs (buy or sell) == 1 --> i can buy;
     // bs (buy or sell) == 0 --> i can sell;
+
     private int f(int[] prices, int i, int bs, int[][] memo) {
         if (i >= prices.length) {
             return 0;
@@ -29,5 +32,33 @@ class Solution {
                 f(prices, i + 1, 0, memo)
             );
         }
+    }
+
+
+/*--------------------Tabulation--------------------------------------------------------*/
+
+    private int stockProfit(int[] Arr) {
+        int n = Arr.length;
+        int dp[][] = new int[n + 2][2];
+        
+        // Iterate through the array backwards
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+
+                if (buy == 0) { // We can buy the stock
+                    profit = Math.max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
+                }
+
+                if (buy == 1) { // We can sell the stock
+                    profit = Math.max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 2][0]);
+                }
+
+                dp[ind][buy] = profit;
+            }
+        }
+
+        // The maximum profit is stored in dp[0][0]
+        return dp[0][0];
     }
 }
