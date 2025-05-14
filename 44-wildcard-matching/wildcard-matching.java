e73aa422e19187d6680d88c844c32dc06d6b314c
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-14 11:00:39 +0530

    Time: 25 ms (22.05%) | Memory: 49 MB (11.74%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -6,35 +6,35 @@ class Solution {
 /*---------------------------------------------------------------------------------------*/
         int[][] memo = new int[n][m];
         for (int[] x : memo) Arrays.fill(x, -1);
-        // return f(s, p, n - 1, m - 1, memo);
+        return f(s, t, n - 1, m - 1, memo);
 
 
 /*--------------------------tabulation---------------------------------------------------*/
 
-        boolean[][] dp = new boolean[n + 1][m + 1];
-        dp[0][0] = true;
-        for (int i = 1; i <= m; i++) {
-            if (t.charAt(i - 1) == '*') {
-                dp[0][i] = dp[0][i - 1];
-            } else {
-                dp[0][i] = false;
-            }
-        }
+        // boolean[][] dp = new boolean[n + 1][m + 1];
+        // dp[0][0] = true;
+        // for (int i = 1; i <= m; i++) {
+        //     if (t.charAt(i - 1) == '*') {
+        //         dp[0][i] = dp[0][i - 1];
+        //     } else {
+        //         dp[0][i] = false;
+        //     }
+        // }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s.charAt(i- 1) == t.charAt(j - 1) || t.charAt(j - 1) == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (s.charAt(i - 1) != t.charAt(j - 1)) {
-                    if (t.charAt(j - 1) == '*') {
-                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                     } else {
-                        dp[i][j] = false;
-                    }
-                }
-            }
-        }
-        return dp[n][m];
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= m; j++) {
+        //         if (s.charAt(i- 1) == t.charAt(j - 1) || t.charAt(j - 1) == '?') {
+        //             dp[i][j] = dp[i - 1][j - 1];
+        //         } else if (s.charAt(i - 1) != t.charAt(j - 1)) {
+        //             if (t.charAt(j - 1) == '*') {
+        //                 dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+        //              } else {
+        //                 dp[i][j] = false;
+        //             }
+        //         }
+        //     }
+        // }
+        // return dp[n][m];
     }
 
 
