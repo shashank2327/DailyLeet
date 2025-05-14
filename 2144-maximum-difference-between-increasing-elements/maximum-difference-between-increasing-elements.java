624f8a066a7bdeefce3940ed365f4a8ab6094c34
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-14 23:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (32.59%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -1,23 +1,12 @@
 class Solution {
-    public int maximumDifference(int[] nums) {
-        int n = nums.length;
-        int[] min = new int[n];
-        Arrays.fill(min, Integer.MAX_VALUE);
-        int[] max = new int[n];
-
-        min[0] = nums[0];
-        max[n - 1] = nums[n - 1];
-
+    public int maximumDifference(int[] prices) {
+        int n = prices.length;
+        int ans = 0;
+        int min = prices[0];
         for (int i = 1; i < n; i++) {
-            min[i] = Math.min(min[i - 1], nums[i]);
+            if (prices[i] > min) ans = Math.max(ans, prices[i] - min);
+            min = Math.min(prices[i], min);
         }
-        for (int i = n - 2; i >= 0; i--) {
-            max[i] = Math.max(max[i + 1], nums[i]);
-        }
-        int maxDiff = -1;
-        for (int i = 0; i < n - 1; i++) {
-            maxDiff = Math.max(maxDiff, max[i + 1] - min[i]);
-        }
-        return maxDiff == 0 ? -1 : maxDiff;
+        return ans == 0 ? -1 : ans;
     }
 }
