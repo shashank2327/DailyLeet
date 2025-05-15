aeb687797b98aaa4f054e646b55b1e14c2b918c3
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-15 10:08:46 +0530

    Time: 85 ms (21.21%) | Memory: 90.9 MB (5.03%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -9,21 +9,37 @@ class Solution {
 
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
-
         int i = 1;
-        int max = 1;
+        int max = 0;
+        int[] hash = new int[n];
+        for (int k = 0; k < n; k++) hash[k] = k;
         while (i != n) {
             int j = 0;
             while (j < i) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                if (nums[j] < nums[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    hash[i] = j;
                 }
                 j++;
             }
             i++;
         }
 
-        for (int num : dp) max = Math.max(max, num);
+        // Printing LIS
+        int hashIndex = -1;
+        for (int k = 0; k < n; k++) {
+            if (dp[k] > max) {
+                max = dp[k];
+                hashIndex = k;
+            }
+        }
+
+        int[] printLIS = new int[max];
+        for (int k = max - 1; k >= 0; k--) {
+            printLIS[k] = nums[hashIndex];
+            hashIndex = hash[hashIndex];
+        }
+        System.out.println(Arrays.toString(printLIS));
         return max;
     }
 
@@ -38,6 +54,4 @@ class Solution {
         int notTake = f(nums, i, j + 1,memo);
         return memo[i + 1][j] = Math.max(take, notTake);
     }
-
-
 }
