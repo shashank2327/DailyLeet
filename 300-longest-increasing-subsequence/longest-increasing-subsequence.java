25e41d916f0224e6e92d9e96c9e9ff8c4d83d782
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-15 09:47:32 +0530

    Time: 75 ms (24.02%) | Memory: 91.1 MB (5.03%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,11 +1,33 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
+/*-------------------------------------------------------------------------------------------*/        
         int[][] memo = new int[n + 1][n];
         for (int[] x : memo) Arrays.fill(x, -1);
-        return f(nums, -1, 0, memo);
+        // return f(nums, -1, 0, memo);
+/*-------------------------------------------------------------------------------------------*/  
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int i = 1;
+        int max = 1;
+        while (i != n) {
+            int j = 0;
+            while (j < i) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(dp[i], max);
+                }
+                j++;
+            }
+            i++;
+        }
+
+        return max;
     }
 
+/*--------------------------memo + rec-------------------------------------------------------*/
     private int f(int[] nums, int i, int j, int[][] memo) {
         if (j == nums.length) return 0;
         if (memo[i + 1][j] != -1) return memo[i + 1][j];
@@ -15,6 +37,7 @@ class Solution {
         }
         int notTake = f(nums, i, j + 1,memo);
         return memo[i + 1][j] = Math.max(take, notTake);
-
     }
+
+
 }
