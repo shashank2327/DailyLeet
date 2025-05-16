199b70b51f70809fc69f1d57e132abab1faa1893
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-16 20:05:33 +0530

    Time: 2494 ms (31.43%) | Memory: 41.7 MB (69.88%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,9 +1,23 @@
 class Solution {
     public int minCut(String s) {
         int n = s.length();
-        int[] memo = new int[n];
-        Arrays.fill(memo, -1);
-        return f(s, 0, memo) - 1;
+        // int[] memo = new int[n];
+        // Arrays.fill(memo, -1);
+        // return f(s, 0, memo) - 1;
+
+
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            int min = Integer.MAX_VALUE;
+            for (int j = n - 1; j >= i; j--) {
+                if (isPalindrome(s, i, j)) {
+                    int cuts = 1 + dp[j + 1];
+                    min = Math.min(min, cuts);
+                }
+            }
+            dp[i] = min;
+        }
+        return dp[0] -1;
     }
 
 /*------------------------memo + recur-----------------------------------------------------------*/    
