3e59781fa77dc3dbfde9600fb830c01ab1cc6d64
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-29 22:13:35 +0530

    Time: 5 ms (18.54%) | Memory: 43.8 MB (10.37%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        fun(nums, target, 0, 0, -1, new ArrayList<>(), res);
+        return res;
+    }
+    
+    private void fun(
+        int[] nums,
+        int target,
+        int currsum,
+        int i,
+        int j,
+        List<Integer> temp,
+        List<List<Integer>> res
+    ){
+
+        if (currsum == target) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        if (currsum > target || i == nums.length) {
+            return;
+        }
+
+
+        if (i == 0 || !(i != j + 1 && nums[i] == nums[i - 1])) {
+            temp.add(nums[i]);
+            fun(nums, target, currsum + nums[i], i + 1, i, temp, res);
+            temp.remove(temp.size() - 1);
+        }
+
+        fun(nums, target, currsum, i + 1, j, temp, res);
+    }
+}
