b653278243962c777899c6d71e7cfaae9a9dc6d0
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-04 10:02:55 +0530

    Time: 2 ms (29.43%) | Memory: 43.6 MB (29.16%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,80 @@
+public class DisjointSet {
+
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+
+    // Constructor (Taking number of nodes);
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    // Path compression
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+
+    // Finding union using rank;
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_u) > rank.get(ulp_v)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+
+
+    // Finding union using size;
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_u) + size.get(ulp_v));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        DisjointSet ds = new DisjointSet(n);
+
+        for (int i = 0; i < n; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                return new int[]{u, v};
+            }
+            ds.unionBySize(u, v);
+        }
+
+        return new int[]{};
+    }
+}
