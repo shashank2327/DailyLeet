ee6cdbb9ab99ecd425238020a7a45609203b0810
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-04 10:10:30 +0530

    Time: 2 ms (29.43%) | Memory: 43.8 MB (26.88%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,6 +1,4 @@
 public class DisjointSet {
-
-    List<Integer> rank = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
     List<Integer> size = new ArrayList<>();
 
@@ -8,7 +6,6 @@ public class DisjointSet {
     // Constructor (Taking number of nodes);
     public DisjointSet(int n) {
         for (int i = 0; i <= n; i++) {
-            rank.add(0);
             parent.add(i);
             size.add(1);
         }
@@ -22,25 +19,6 @@ public class DisjointSet {
         int ulp = findUPar(parent.get(node));
         parent.set(node, ulp);
         return parent.get(node);
-    }
-
-
-    // Finding union using rank;
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-
-        if (ulp_u == ulp_v) return;
-
-        if (rank.get(ulp_u) < rank.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-        } else if (rank.get(ulp_u) > rank.get(ulp_v)) {
-            parent.set(ulp_v, ulp_u);
-        } else {
-            parent.set(ulp_v, ulp_u);
-            int rankU = rank.get(ulp_u);
-            rank.set(ulp_u, rankU + 1);
-        }
     }
 
 
