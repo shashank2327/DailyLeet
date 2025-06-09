001cc6eac3cffa520a87edae1679c2a5d5dbb100
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-09 10:26:42 +0530

    Time: 4 ms (98.91%) | Memory: 45.9 MB (45.53%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.java
+++ 1738-maximal-network-rank/maximal-network-rank.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
         int[] outdegree = new int[n];
+        boolean[][] connected = new boolean[n][n];
         int m = roads.length;
-        Set<String> set = new HashSet<>();
         for (int i = 0; i < m; i++) {
             int u = roads[i][0];
             int v = roads[i][1];
-            set.add(u + "," + v);
-            set.add(v + "," + u);
+            connected[u][v] = true;
+            connected[v][u] = true;
             outdegree[u]++;
             outdegree[v]++;
         }
         int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (set.contains(i + "," + j)) {
+                if (connected[i][j]) {
                     ans = Math.max(ans, outdegree[i] + outdegree[j] - 1);
                 } else {
                     ans = Math.max(ans, outdegree[i] + outdegree[j]);
