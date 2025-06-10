52b00f6d3ec75d86c6b8d105561614faa9f81fca
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-10 09:40:01 +0530

    Time: 14 ms (77.30%) | Memory: 45.2 MB (94.95%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -14,27 +14,25 @@ class Solution {
                 }
             }
         }
-
-        int maxDis = -1;
+        if (q.isEmpty() || q.size() == n * n) return -1;
+        int steps = 0;
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, 1, -1};
         while (!q.isEmpty()) {
-            // int sz = q.size();
-            // while (sz-- > 0) {
+            int sz = q.size();
+            while (sz-- > 0) {
                 int[] it = q.poll();
                 for (int i = 0; i < 4; i++) {
                     int nr = dx[i] + it[0];
                     int nc = dy[i] + it[1];
-                    if (nr >= 0 && nc >= 0 && nr < n && nc < n) {
-                        if (dis[it[0]][it[1]] + 1 < dis[nr][nc]) {
-                            dis[nr][nc] = dis[it[0]][it[1]] + 1;
-                            maxDis = Math.max(maxDis, dis[nr][nc]);
-                            q.offer(new int[]{nr, nc});
-                        }
+                    if (nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0) {
+                        grid[nr][nc] = 1;
+                        q.offer(new int[]{nr, nc});
                     }
                 }
-            // }
+            }
+            steps++;
         }
-        return maxDis;
+        return steps - 1;
     }
 }
