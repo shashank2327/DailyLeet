939e4356b57b5374014dc72696fccb5272435ae7
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-11 21:41:30 +0530

    Time: 495 ms (12.50%) | Memory: 56.2 MB (5.00%) - LeetSync

--- 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.java
+++ 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.java
@@ -1,33 +1,21 @@
 class Solution {
-    private int[][] prefixFreq;
-
     public int minimumSubstringsInPartition(String s) {
-        int n = s.length();
-        int[][] memo = new int[n][n];
+        int[][] memo = new int[s.length()][s.length()];
         for (int[] x : memo) Arrays.fill(x, -1);
-        
-        prefixFreq = new int[n][26];
-        prefixFreq[0][s.charAt(0) - 'a'] = 1;
-
-        for (int i = 1; i < n; i++) {
-            for (int c = 0; c < 26; c++) {
-                prefixFreq[i][c] = prefixFreq[i - 1][c];
-            }
-            prefixFreq[i][s.charAt(i) - 'a']++;
-        }
-
-        return fun(s, 0, n - 1, memo);
+        return fun(s, 0, s.length() - 1, memo);
     }
-
     private int fun(String s, int i, int j, int[][] memo) {
         if (i > j) return 0;
-        if (i == j) return 1;
+        if (i == j) {
+            return 1;
+        }
         if (memo[i][j] != -1) return memo[i][j];
-
         int ans = Integer.MAX_VALUE;
-
+        int[] freq = new int[26];
         for (int k = i; k <= j; k++) {
-            if (isBalanced(i, k)) {
+            // String str = s.substring(i, k + 1);
+            freq[s.charAt(k) - 'a']++;
+            if (check(freq)) {
                 ans = Math.min(ans, 1 + fun(s, k + 1, j, memo));
             }
         }
@@ -35,21 +23,16 @@ class Solution {
         return memo[i][j] = ans;
     }
 
-    private boolean isBalanced(int i, int j) {
-        int[] freq = new int[26];
-
-        for (int c = 0; c < 26; c++) {
-            freq[c] = prefixFreq[j][c] - (i > 0 ? prefixFreq[i - 1][c] : 0);
-        }
-
-        int count = 0;
-        for (int f : freq) {
-            if (f > 0) {
-                if (count == 0) count = f;
-                else if (count != f) return false;
+    private boolean check(int[] freq) {
+        int max = 0;
+        int min = 1001;
+        for (int num : freq) {
+            if (num > 0) {
+                max = Math.max(max, num);
+                min = Math.min(min, num);
             }
         }
 
-        return true;
+        return max == min;
     }
 }
