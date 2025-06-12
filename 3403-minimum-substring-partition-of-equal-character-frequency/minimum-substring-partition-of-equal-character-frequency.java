ef5e1c5eb0cb0468449cfbe875c8cdbb6bc81fc8
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-12 07:58:43 +0530

    Time: 469 ms (12.50%) | Memory: 45 MB (46.25%) - LeetSync

--- 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.java
+++ 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.java
@@ -1,26 +1,26 @@
 class Solution {
     public int minimumSubstringsInPartition(String s) {
-        int[][] memo = new int[s.length()][s.length()];
-        for (int[] x : memo) Arrays.fill(x, -1);
+        int[] memo = new int[s.length()];
+        Arrays.fill(memo, -1);
         return fun(s, 0, s.length() - 1, memo);
     }
-    private int fun(String s, int i, int j, int[][] memo) {
-        if (i > j) return 0;
-        if (i == j) {
+    private int fun(String s, int i, int n, int[] memo) {
+        if (i > n) return 0;
+        if (i == n) {
             return 1;
         }
-        if (memo[i][j] != -1) return memo[i][j];
+        if (memo[i] != -1) return memo[i];
         int ans = Integer.MAX_VALUE;
         int[] freq = new int[26];
-        for (int k = i; k <= j; k++) {
+        for (int k = i; k <= n; k++) {
             // String str = s.substring(i, k + 1);
             freq[s.charAt(k) - 'a']++;
             if (check(freq)) {
-                ans = Math.min(ans, 1 + fun(s, k + 1, j, memo));
+                ans = Math.min(ans, 1 + fun(s, k + 1, n, memo));
             }
         }
 
-        return memo[i][j] = ans;
+        return memo[i] = ans;
     }
 
     private boolean check(int[] freq) {
