7a690b179431f89e31c0db89e04ca8385af310b7
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-13 12:45:21 +0530

    Time: 70 ms (20.09%) | Memory: 45.5 MB (39.06%) - LeetSync

--- 87-scramble-string/scramble-string.java
+++ 87-scramble-string/scramble-string.java
@@ -2,14 +2,6 @@ class Solution {
     Map<String, Boolean> map = new HashMap<>();
     public boolean isScramble(String s1, String s2) {
 
-        if (s1.length() != s2.length()) return false;
-        int[] freq = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            freq[s1.charAt(i) - 'a']++;
-            freq[s2.charAt(i) - 'a']--;
-        }
-        for (int num : freq) if (num != 0) return false;
-
         return fun(s1, s2);
     }
 
