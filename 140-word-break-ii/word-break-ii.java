232f3efa37c09f64a1c990f61dbb9505f9c13e90
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-13 10:04:45 +0530

    Time: 4 ms (52.74%) | Memory: 42.2 MB (16.91%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -2,7 +2,8 @@ class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         List<String> ls = new ArrayList<>();
         Set<String> set = new HashSet<>(wordDict);
-        dfs(s, 0, set, "", ls);
+        Map<Integer, String> memo = new HashMap<>();
+        dfs(s, 0, set, "", ls, memo);
 
         // for (int i = 0; i < ls.size(); i++) {
         //     System.out.println(ls.get(i));
@@ -15,7 +16,8 @@ class Solution {
         int start,
         Set<String> wordSet,
         String temp,
-        List<String> ls
+        List<String> ls, 
+        Map<Integer, String> memo
     ) {
         // base case: traversed the whole String;
         if (start == s.length()) {  
@@ -23,12 +25,16 @@ class Solution {
             ls.add(trimmed);
             return;
         }
+        
+        if (memo.containsKey(start) && memo.get(start) == temp) return;
 
         for (int end = start + 1; end <= s.length(); end++) {
             String prefix = s.substring(start, end);
             if (wordSet.contains(prefix)) {
-                dfs(s, end, wordSet, temp + prefix + " ", ls);
+                dfs(s, end, wordSet, temp + prefix + " ", ls, memo);
             }
         }
+
+        memo.put(start, temp);
     }
 }
