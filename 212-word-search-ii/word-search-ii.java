ac03af897b909815b63b7012a554b721debd0811
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-14 10:50:12 +0530

    Time: 204 ms (64.35%) | Memory: 44.5 MB (98.04%) - LeetSync

--- 212-word-search-ii/word-search-ii.java
+++ 212-word-search-ii/word-search-ii.java
@@ -1,125 +1,71 @@
 class Node {
-    Node[] links;
-    boolean flag;
-
-    public Node() {
-        links = new Node[26];
-        flag = false;
-    }
-
-    boolean contains(char ch) {
-        return (links[ch - 'a'] != null);
-    }
-
-    void put(char ch) {
-        links[ch - 'a'] = new Node();
-    }
-
-    Node get(char ch) {
-        return links[ch - 'a'];
-    }
-
-    void setEnd() {
-        flag = true;
-    }
-
-    boolean isEnd() {
-        return flag;
-    }
+    Node[] links = new Node[26];
+    boolean isEnd = false;
+    String word = null; // Store full word at terminal node for fast retrieval
 }
 
 class Trie {
+    Node root = new Node();
 
-    private final Node root;
-
-    public Trie() {
-        root = new Node();
-    }
-
-    public void insert(String word) {
+    void insert(String word) {
         Node node = root;
-        for (int i = 0; i < word.length(); i++) {
-            if (!node.contains(word.charAt(i))) {
-                node.put(word.charAt(i));
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (node.links[idx] == null) {
+                node.links[idx] = new Node();
             }
-            node = node.get(word.charAt(i));
+            node = node.links[idx];
         }
-        node.setEnd();
-    }
-    
-    public boolean search(String word) {
-        Node node = root;
-        for (int i = 0; i < word.length(); i++) {
-            if (!node.contains(word.charAt(i))) {
-                return false;
-            }
-            node = node.get(word.charAt(i));
-        }
-        return node.isEnd();
-    }
-
-    public boolean startsWith(String prefix) {
-        Node node = root;
-        for (int i = 0; i < prefix.length(); i++) {
-            if (!node.contains(prefix.charAt(i))) {
-                return false;
-            }
-            node = node.get(prefix.charAt(i));
-        }
-        return true;
+        node.isEnd = true;
+        node.word = word;
     }
 }
 
 class Solution {
-
     int[] dx = {1, -1, 0, 0};
     int[] dy = {0, 0, 1, -1};
 
     public List<String> findWords(char[][] board, String[] words) {
-        int n = board.length, m = board[0].length;
-        List<String> ls = new ArrayList<>();
-        Set<String> set = new HashSet<>();
         Trie trie = new Trie();
-        for (String word: words) {
+        for (String word : words) {
             trie.insert(word);
         }
+
+        Set<String> result = new HashSet<>();
+        int n = board.length, m = board[0].length;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                boolean[][] vis = new boolean[n][m];
-                vis[i][j] = true;
-                dfs(board, i, j, vis, trie, ls, set,  "" + board[i][j]);
+                dfs(board, i, j, trie.root, result);
             }
         }
 
-        return ls;
+        return new ArrayList<>(result);
     }
 
-    private void dfs(
-        char[][] board, 
-        int i, 
-        int j, 
-        boolean[][] vis, 
-        Trie trie, 
-        List<String> ls,
-        Set<String> set,
-        String s
-    ) 
-    {
-        if (!trie.startsWith(s)) return;
+    private void dfs(char[][] board, int i, int j, Node node, Set<String> result) {
+        char ch = board[i][j];
 
-        if (trie.search(s) && !set.contains(s)) {
-            ls.add(s);
-            set.add(s);
+        if (ch == '#' || node.links[ch - 'a'] == null) return;
+
+        node = node.links[ch - 'a'];
+
+        if (node.isEnd) {
+            result.add(node.word);
+            node.isEnd = false;
         }
 
-        for (int k = 0; k < 4; k++) {
-            int nr = i + dx[k];
-            int nc = j + dy[k];
-            if (nr >= 0 && nc >= 0 && nr < board.length && nc < board[0].length && !vis[nr][nc]) {
-                vis[nr][nc] = true;
-                dfs(board, nr, nc, vis, trie, ls,set,  s + board[nr][nc]);
-                vis[nr][nc] = false;
+        board[i][j] = '#'; // Mark visited
+
+        for (int dir = 0; dir < 4; dir++) {
+            int ni = i + dx[dir];
+            int nj = j + dy[dir];
+
+            if (ni >= 0 && nj >= 0 && ni < board.length && nj < board[0].length) {
+                dfs(board, ni, nj, node, result);
             }
-        } 
+        }
+
+        board[i][j] = ch; // Unmark
     }
 }
