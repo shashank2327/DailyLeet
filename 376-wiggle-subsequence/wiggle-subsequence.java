c194f76f87bfb8770f4278877e47727dc4485b66
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-16 20:15:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (23.29%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.java
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -1,46 +1,46 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
         int n = nums.length;
-        int[][][] memo = new int[n + 1][n + 1][3];
-        for (int[][] mat : memo) {
-            for (int[] x : mat) {
-                Arrays.fill(x, -1);
+        if (n == 1) {
+            return n;
+        } else if (n == 2) {
+            if (nums[0] != nums[1]) {
+                return 2;
+            } else {
+                return 1;
             }
         }
-        return wiggleLength(nums, 0, -1, 0, memo);
-    }
-
-    private int wiggleLength(
-        int[] nums,
-        int i, 
-        int j, 
-        int flag,
-        int[][][] memo
-    ) 
-    {
-        if (i == nums.length) {
-            return 0;
-        }
-        if (memo[i + 1][j + 1][flag + 1] != -1) {
-            return memo[i + 1][j + 1][flag + 1];
-        }
-
-        int ans = Integer.MIN_VALUE;
-        if (flag == 0) {
-            if (j == -1) {
-                ans = Math.max(ans, 1 + wiggleLength(nums, i + 1, i, 0, memo));
-            } else if (nums[i] != nums[j]) {
-                int myFlag = nums[i] - nums[j] > 0 ? 1 : -1;
-                ans = Math.max(ans, 1 + wiggleLength(nums, i + 1, i, myFlag, memo));
+        int i = 0;
+        int prev = 0;
+        while (i < n - 1) {
+            if (nums[i + 1] != nums[i]) {
+                prev = nums[i + 1] - nums[i];
+                break;
             }
-        } else if (flag == 1 && nums[i] - nums[j] < 0) {
-            ans = Math.max(ans, 1 + wiggleLength(nums, i + 1, i, -1, memo));
-        } else if (flag == -1 && nums[i] - nums[j] > 0) {
-            ans = Math.max(ans, 1 + wiggleLength(nums, i + 1, i, 1, memo));
+            i++;
         }
 
-        ans = Math.max(ans, wiggleLength(nums, i + 1, j, flag, memo));
+        if (i == n - 1) {
+            return 1;
+        }
 
-        return memo[i + 1][j + 1][flag + 1] = ans;
+        int cnt = 0;
+        i += 2;
+
+        while (i < n) {
+            int curr = nums[i] - nums[i - 1];
+            if (curr == 0) {
+                i++;
+                continue;
+            }
+            if ((curr > 0 && prev < 0) || (curr < 0 && prev > 0)) {
+                prev = curr;
+                cnt++;
+            }
+            i++;
+        }
+        
+        return cnt + 2;
+        // return 0;
     }
 }
