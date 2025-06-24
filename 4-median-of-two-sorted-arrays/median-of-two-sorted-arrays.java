7c89cd37b1cb017414624433b41d986572714709
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-24 20:13:53 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (12.02%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,35 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-
-        int p1 = 0;
-        int p2 = 0;
-        double[] res = new double[n + m];
-        int idx = 0;
-        while (p1 < n && p2 < m) {
-            if (nums1[p1] <= nums2[p2]) {
-                res[idx++] = (double)nums1[p1];
-                p1++;
-            } else {
-                res[idx++] = (double)nums2[p2];
-                p2++;
+        int n1 = nums1.length;
+        int n2 = nums2.length; 
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int low = 0;
+        int high = n1;
+        int total = (n1  + n2 + 1) /2;
+        int n = n1 + n2;
+        while (low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = total - mid1;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE; 
+            if (mid1 < n1) r1 = nums1[mid1];
+            if (mid2 < n2) r2 = nums2[mid2];
+            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return Math.max(l1, l2);
+                return (double)(Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else if (l2 > r1) {
+                low = mid1 + 1;
             }
         }
-
-        while (p1 < n) {
-            res[idx++] = (double)nums1[p1];
-            p1++;
-        }
-        while (p2 < m) {
-            res[idx++] = (double)nums2[p2];
-            p2++;
-        }
-
-        if ((n + m) % 2 != 0) {
-            return res[(n+m)/2];
-        } else {
-            return (res[(n + m) / 2] + res[(n + m)/2  - 1]) /2;
-        }
+        return 0;
     }
 }
