a60396d1e481d0c99e68950411ea941c012d4280
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-25 10:48:25 +0530

    Time: 17 ms (24.80%) | Memory: 62.3 MB (44.92%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,21 +1,15 @@
-public class DisjointSet {
+class DisjointSet {
 
-    List<Integer> rank = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
     List<Integer> size = new ArrayList<>();
 
-
-    // Constructor (Taking number of nodes);
     public DisjointSet(int n) {
         for (int i = 0; i <= n; i++) {
-            rank.add(0);
             parent.add(i);
             size.add(1);
         }
     }
 
-
-    // Path compression
     public int findUPar(int node) {
         if (node == parent.get(node)) {
             return node;
@@ -25,32 +19,13 @@ public class DisjointSet {
         return parent.get(node);
     }
 
-
-    // Finding union using rank;
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-
-        if (ulp_u == ulp_v) return;
-
-        if (rank.get(ulp_u) < rank.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-        } else if (rank.get(ulp_u) > rank.get(ulp_v)) {
-            parent.set(ulp_v, ulp_u);
-        } else {
-            parent.set(ulp_v, ulp_u);
-            int rankU = rank.get(ulp_u);
-            rank.set(ulp_u, rankU + 1);
-        }
-    }
-
-
-    // Finding union using size;
     public void unionBySize(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
 
-        if (ulp_u == ulp_v) return;
+        if (ulp_u == ulp_v) {
+            return;
+        }
 
         if (size.get(ulp_u) < size.get(ulp_v)) {
             parent.set(ulp_u, ulp_v);
@@ -62,30 +37,39 @@ public class DisjointSet {
     }
 }
 
-
 class Solution {
     public int makeConnected(int n, int[][] connections) {
+        int edges = connections.length;
+        if (edges < n - 1) return -1;
+
         DisjointSet ds = new DisjointSet(n);
         int extra = 0;
-        int m = connections.length;
-        for (int i = 0; i < m; i++) {
-            int u = connections[i][0];
-            int v = connections[i][1];
-            if (ds.findUPar(u) == ds.findUPar(v)) { 
+        for (int[] cable : connections) {
+            int comp1 = cable[0];
+            int comp2 = cable[1];
+
+            if (ds.findUPar(comp1) == ds.findUPar(comp2)) {
                 extra++;
             } else {
-                ds.unionBySize(u, v);
+                ds.unionBySize(comp1, comp2);
             }
         }
 
-        int cntC = 0;
+        int disconnected = 0;
+
         for (int i = 0; i < n; i++) {
-            if (ds.parent.get(i) == i) cntC++;
+            if (ds.parent.get(i) == i) disconnected++;
         }
 
-        int ans = cntC - 1;  // reason for subtracting 1 ->> There will be one ultimate Parent.
+        int req = disconnected - 1;
 
-        if (extra >= ans) return ans;
-        return -1;
+        if (extra >= req) {
+            return req;
+        } else {
+            return -1;
+        }
     }
 }
+
+
+// for -1 -> if number of comp is n, then minimum n - 1 edges should be there;
