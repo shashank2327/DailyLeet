17e2d9ff59a53dff8c9e3f7122efc9a81b1dc178
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-25 16:30:03 +0530

    Time: 7 ms (91.75%) | Memory: 44.8 MB (32.60%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,60 +1,63 @@
-class Tuple {
-    int time;
-    int row;
-    int col;
-
-    public Tuple(int time, int row, int col) {
-        this.time = time;
-        this.row = row;
-        this.col = col;
-    }
-}
-
 class Solution {
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        
-        int[][] tm = new int[n][n];
-        for (int[] x : tm) {
-            Arrays.fill(x, (int)1e9);
-        }
-        tm[0][0] = grid[0][0];
+	int max = -1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			max = Math.max(max, grid[i][j]);	
+		}
+}
+int lo = 0;
+int hi = max;
+int result = -1;
+while (lo <= hi) {
+		int mid = lo + (hi - lo)/ 2;
+		boolean flag = bfs(grid, mid);
+		if ( flag ) {
+			result = mid;
+			hi = mid - 1;
+		} else {
+			lo = mid + 1;
+		}
+}
 
-        PriorityQueue<Tuple> q = new PriorityQueue<>((x, y) -> x.time - y.time);
-        q.add(new Tuple(grid[0][0], 0, 0));
+return result;
 
-        while (!q.isEmpty()) {
-            Tuple it = q.poll();
-            int t = it.time;
-            int r = it.row;
-            int c = it.col;
-
-            if (t > tm[r][c]) continue;
-
-            if (r == n - 1 && c == n - 1) return tm[r][c];
-
-            int[] dx = {0, 0, 1, -1};
-            int[] dy = {1, -1, 0, 0};
-
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dx[i];
-                int nc = c + dy[i];
-                if (nr >= 0 && nc >= 0 && nr < n && nc < n) {
-                    if (grid[nr][nc] <= t) {
-                        if (t < tm[nr][nc]) {
-                            tm[nr][nc] = t;
-                            q.add(new Tuple(t, nr, nc));
-                        }
-                    } else {
-                        if (grid[nr][nc] < tm[nr][nc]) {
-                            tm[nr][nc] = grid[nr][nc];
-                            q.add(new Tuple(grid[nr][nc], nr, nc));
-                        }
-                    }
-                }
-            }
-        }
-
-        return tm[n - 1][n - 1];
     }
+
+    public boolean bfs(int[][] grid, int mid) {
+	if (mid < grid[0][0]) return false;
+	int n = grid.length;
+	int[] dx = {-1, 1, 0, 0};
+	int[] dy = {0, 0, 1, -1};
+	Queue<int[]> q = new LinkedList<>(); // it will store row and col;
+	boolean[][] vis = new boolean[n][n];
+	q.offer(new int[]{0, 0});
+	vis[0][0] = true;
+	
+	while (!q.isEmpty()) {
+		int[] it = q.poll();
+		int row = it[0];
+		int col = it[1];
+		
+
+		if (row == n -1 && col == n - 1) return true;
+		
+		// traverse in all 4 directions
+		for (int i = 0; i < 4; i++) {
+			int nr = row + dx[i];
+			int nc = col + dy[i];
+			if (nr >= 0 && nc >= 0 && nc < n && nr < n && grid[nr][nc] <= mid 
+	&& !vis[nr][nc]
+) {
+				vis[nr][nc] = true;
+				q.offer(new int[]{nr, nc});
+			}
+		}	
+		
+}
+
+return false;
+}
+
 }
