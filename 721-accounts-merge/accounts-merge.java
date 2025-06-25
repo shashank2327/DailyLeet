9f0304bdee05d4e91778592dbe56689625ffe55b
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-25 11:56:37 +0530

    Time: 29 ms (77.59%) | Memory: 47.4 MB (92.62%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -67,7 +67,7 @@ class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         int n = accounts.size();
         DisjointSet ds = new DisjointSet(n);
-        HashMap<String, Integer> mailToNode = new HashMap<>();
+        Map<String, Integer> mailToNode = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < accounts.get(i).size(); j++) {
@@ -94,14 +94,12 @@ class Solution {
 
         List<List<String>> ans = new ArrayList<>();
 
-        for (int i = 0 ; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             if (mergedMail[i].size() == 0) continue;
             Collections.sort(mergedMail[i]);
             List<String> temp = new ArrayList<>();
             temp.add(accounts.get(i).get(0));
-            for (String it : mergedMail[i]) {
-                temp.add(it);
-            }
+            temp.addAll(mergedMail[i]);
             ans.add(temp);
         }
 
