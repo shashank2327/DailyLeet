163636992cf79760360ba81cea1c93feb61790e7
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-27 15:05:29 +0530

    Time: 1 ms (51.66%) | Memory: 43 MB (30.10%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,28 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        fun(nums, 0, new ArrayList<>(), res);
-        return res;
+        List<List<Integer>> result = new ArrayList<>();
+        fun(nums, 0, new ArrayList<>(), result);
+        return result;
     }
+    public void fun(int[] nums, int idx, List<Integer> temp, List<List<Integer>> res) {
+        res.add(new ArrayList<>(temp));
 
-    private void fun(int[] nums, int idx, List<Integer> temp, List<List<Integer>> res) {
-        if (idx == nums.length) {
-            // add the copy of temp in result list.
-            res.add(new ArrayList<>(temp));
-            return;
+        for (int i = idx; i < nums.length; i++) {
+            temp.add(nums[i]);
+            fun(nums, i + 1,  temp, res);
+            temp.remove(temp.size() - 1);
         }
-
-        // pick and call the function for next idx;
-        temp.add(nums[idx]);
-
-        fun(nums, idx + 1, temp, res);
-
-        // When the func returns again to this idx.
-        // take the case of not picking up;
-
-        // removed the current element since we have earlier picked it up
-        temp.remove(temp.size() - 1);
-
-        fun(nums, idx + 1, temp, res);
     }
 }
