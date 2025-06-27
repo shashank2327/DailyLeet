4d5e91f794f582b28cd1d4f509e6ffd82f0125a8
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-27 15:21:08 +0530

    Time: 1 ms (99.88%) | Memory: 43.2 MB (95.63%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,31 +1,18 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        fun(nums, 0, -1, res, new ArrayList<>());
-        return res;
+        List<List<Integer>> result = new ArrayList<>();
+        fun(nums, 0, new ArrayList<>(), result);
+        return result;
     }
+    public void fun(int[] nums, int idx, List<Integer> temp, List<List<Integer>> res) {
+        res.add(new ArrayList<>(temp));
 
-    private void fun(int[] nums, int currIdx, int prevIdx, List<List<Integer>> res, List<Integer> temp) {
-
-        if (currIdx == nums.length) {
-            // add the copy of temp in result list;
-            res.add(new ArrayList<>(temp));
-            return;
-        }
-
-        // I will only take if nums[i] != nums[i - 1];
-        // because if nums[i] == nums[i - 1];
-        // the earlier occurred same element had produced all the combination with remaing elements;
-
-        if (currIdx == 0 || !(currIdx != prevIdx + 1 && nums[currIdx] == nums[currIdx - 1])) {
-            temp.add(nums[currIdx]);
-            fun(nums, currIdx + 1, currIdx, res, temp);
+        for (int i = idx; i < nums.length; i++) {
+            if (i > idx && nums[i] == nums[i - 1]) continue;
+            temp.add(nums[i]);
+            fun(nums, i + 1,  temp, res);
             temp.remove(temp.size() - 1);
         }
-
-
-        // not take case;
-        fun(nums, currIdx + 1, prevIdx, res, temp);
     }
 }
