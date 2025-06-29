980e3f7a88121dadf040483ea80494d84ca833ad
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-29 08:56:44 +0530

    Time: 20 ms (53.28%) | Memory: 45.7 MB (9.46%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,10 +1,10 @@
 class Tuple {
-    int dis;
+    int cost;
     int row;
     int col;
 
-    public Tuple (int dis, int row, int col) {
-        this.dis = dis;
+    public Tuple(int cost, int row, int col) {
+        this.cost = cost;
         this.row = row;
         this.col = col;
     }
@@ -14,46 +14,50 @@ class Solution {
     public int minCost(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
-
-        int[][] dis = new int[n][m];
-        for (int[] x : dis) {
+        int[][] minCost = new int[n][m];
+        for (int[] x : minCost) {
             Arrays.fill(x, (int)1e9);
         }
-        dis[0][0] = 0;
+        PriorityQueue<Tuple> pq = new PriorityQueue<>((t1, t2) -> t1.cost - t2.cost);
 
-        PriorityQueue<Tuple> pq = new PriorityQueue<>((x, y) -> x.dis - y.dis);
-        pq.add(new Tuple(0, 0, 0));
-
+        pq.offer(new Tuple(0, 0, 0)); // I am on (0, 0) cell with cost = 0;
+        minCost[0][0] = 0;
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
         while (!pq.isEmpty()) {
-            int currDis = pq.peek().dis;
-            int r = pq.peek().row;
-            int c = pq.peek().col;
-            pq.remove();
-            
-            if (r == n - 1 && c == m - 1) {
-                return dis[n - 1][m - 1];
-            }
+            Tuple it = pq.poll();
+            int currCost = it.cost;
+            int row = it.row;
+            int col = it.col;
 
             for (int i = 0; i < 4; i++) {
-                int nrow = r + dx[i];
-                int ncol = c + dy[i];
-
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) {
-                    
-                    int nCost = currDis + (i != (grid[r][c] - 1) ? 1 : 0);
-
-                    if (dis[nrow][ncol] > nCost) {
-                        dis[nrow][ncol] = nCost;
-                        pq.offer(new Tuple(nCost, nrow, ncol));
+                if (i + 1 == grid[row][col]) { // going in the same direction
+                    int nr = dx[i] + row;
+                    int nc = dy[i] + col;
+                    if (nr >= 0 && nc >= 0 && nr < n && nc < m && minCost[nr][nc] > currCost) {
+                        minCost[nr][nc] = currCost;
+                        pq.offer(new Tuple(currCost, nr, nc));
+                    }
+                } else {
+                    int nr = dx[i] + row;
+                    int nc = dy[i] + col;
+                    if (nr >= 0 && nc >= 0 && nr < n && nc < m && minCost[nr][nc] > currCost + 1) {
+                        minCost[nr][nc] = currCost + 1;
+                        pq.offer(new Tuple(currCost + 1, nr, nc));
                     }
                 }
             }
         }
-
-        return 0;
+        return minCost[n - 1][m - 1];
     }
 }
+// for every cell try out all four directions;
+// if going in the same direction as specified by cell (cost + 0);
+// else (cost + 1);
 
+
+/* right (row, col + 1)  // {0, 0, 1, -1};   // dy = {1, -1, 0, 0};
+   left  (row, col - 1)
+   down  (row + 1, col)
+   up    (row - 1, col)
+   */
