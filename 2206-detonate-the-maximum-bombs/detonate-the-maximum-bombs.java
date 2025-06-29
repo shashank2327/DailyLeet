38717d6d5fd7d90463a72a2efd1251569ba1102a
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-06-29 07:32:11 +0530

    Time: 81 ms (83.08%) | Memory: 45.1 MB (45.46%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -1,60 +1,69 @@
 class Solution {
+    public int maximumDetonation(int[][] bombs) {
+        int numberOfBombs = bombs.length;
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < numberOfBombs; i++) {
+            adjList.add(new ArrayList<>());
+        }
 
-    private int bfs(int src, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
-        
-        int sum  = 1;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(src);
-        vis[src] = true;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            for (int it : adj.get(node)) {
-                if (!vis[it]) {
-                    sum++;
-                    vis[it] = true;
-                    q.add(it);
+        for (int i = 0; i < numberOfBombs; i++) {
+            for (int j = 0; j < numberOfBombs; j++) {
+                if (i == j) continue;
+                if (inRange(bombs, i, j)) {
+                    adjList.get(i).add(j);
                 }
             }
         }
 
-        return sum;
-    }
-
-    public int maximumDetonation(int[][] bombs) {
-        int n = bombs.length;
-
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+        boolean[] vis = new boolean[numberOfBombs];
+        int maxDetonation = 0;
+        for (int i = 0; i < numberOfBombs; i++) {
+            vis = new boolean[numberOfBombs];
+            if (!vis[i]) {
+                int detonations = bfs(adjList, i, vis);
+                maxDetonation = Math.max(maxDetonation, detonations);
+            }
         }
 
+        return maxDetonation;
+    }
 
-        for (int i = 0; i < n; i++) {
-            int cnt = 1;
-            for (int j = 0; j < n; j++) {
-                if (i != j) {
-                    long x = Math.abs(bombs[i][0] - bombs[j][0]);
-                    long y = Math.abs(bombs[i][1] - bombs[j][1]);
-                    long r = bombs[i][2];
-                    if ((x * x + y * y) <= r * r) {
-                        adj.get(i).add(j);
+    private int bfs(List<List<Integer>> adjList, int node, boolean[] vis) {
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 1;
+        vis[node] = true;
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            int sz = queue.size();
+            for (int i = 0; i < sz; i++) {
+                int nde = queue.poll();
+                for (int adjNode : adjList.get(nde)) {
+                    if (!vis[adjNode]) {
+                        count++;
+                        queue.offer(adjNode);
+                        vis[adjNode] = true;
                     }
                 }
             }
         }
+        return count;
+    }
 
-        boolean[] vis = new boolean[n];
-        int ans = 1;
 
-        for (int i = 0; i < n; i++) {
-            vis = new boolean[n];
-            if (!vis[i]) {
-                int res = bfs(i, adj, vis);
-                ans = Math.max(res, ans);
-            }
+    private boolean inRange(int[][] bombs, int i, int j) {
+        int x1 = bombs[i][0], y1 = bombs[i][1], r1 = bombs[i][2]; // info of bomb1;
+        int x2 = bombs[j][0], y2 = bombs[j][1], r2 = bombs[j][2]; // info of bomb2;
+
+        long maxRadiusSquare = (long)Math.pow(r1, 2);
+        long xAbs = (long)Math.abs(x1 - x2);
+        long yAbs = (long)Math.abs(y1 - y2);
+        long distanceSquare = (xAbs * xAbs) + (yAbs * yAbs);
+
+        if (maxRadiusSquare >= distanceSquare) {
+            return true;
         }
-        
-        return ans;
+        return false;
     }
 }
+// when can we say one bomb comes in another bomb range;
+// when the distance between two bombs is <= Math.max(r1, r2);
