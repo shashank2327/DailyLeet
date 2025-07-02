a143d5f07c5b70a0c240e00522f1282f8e6fcf20
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-02 23:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (64.59%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,45 +1,46 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-        if (needle.isEmpty()) {
-            return 0;
-        }
+    public int strStr(String aStr, String bStr) {
+        int[] lps = prefixSuffix(bStr.toCharArray());
 
-            // Compute the LPS array
-        int[] lps = new int[needle.length()];
-        int prevLPS = 0, i = 1;
-        while (i < needle.length()) {
-            if (needle.charAt(i) == needle.charAt(prevLPS)) {
-                lps[i] = prevLPS + 1;
-                prevLPS++;
-                i++;
-            } else {
-                if (prevLPS == 0) {
-                    lps[i] = 0;
-                    i++;
-                } else {
-                    prevLPS = lps[prevLPS - 1];
-                }
-            }
-        }
-
-            // Search for the needle in the haystack
-        int j = 0; // Pointer for needle
-        i = 0; // Pointer for haystack
-        while (i < haystack.length()) {
-            if (haystack.charAt(i) == needle.charAt(j)) {
+        int i = 0;
+        int j = 0;
+        char[] a = aStr.toCharArray();
+        char[] b = bStr.toCharArray();
+        while (i < a.length && j < b.length) {
+            if (a[i] == b[j]) {
                 i++;
                 j++;
             } else {
-                if (j == 0) {
-                    i++;
-                } else {
+                if (j != 0) {
                     j = lps[j - 1];
+                } else {
+                    i++;
                 }
             }
-            if (j == needle.length()) {
-                return i - needle.length();
+        }
+        if (j != b.length) return -1;
+        return i - j;
+    }
+
+    private int[] prefixSuffix(char[] pattern) {
+        int[] lps = new int[pattern.length];
+        int index = 0;
+
+        for (int i = 1; i < pattern.length;) {
+            if (pattern[i] == pattern[index]) {
+                lps[i] = index + 1;
+                index++;
+                i++;
+            } else {
+                if (index != 0) {
+                    index = lps[index - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
-        return -1;
+
+        return lps;
     }
 }
