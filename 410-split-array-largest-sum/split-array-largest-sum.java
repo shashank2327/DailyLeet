78e9321c7ee4f4128d829db03fda35d6938344e0
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-02 10:47:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (54.71%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,39 +1,50 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int ans = binarySearch(nums, k);
-        return ans;
-    }
+        int sum = 0;
+        for (int num : nums) sum += num;
 
-    private int binarySearch(int[] arr, int k) {
-        int n = arr.length;
-        int low = -1;
-        for (int num : arr) {
-            low = Math.max(low, num);
-        }
-        int high = Arrays.stream(arr).sum();
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int partitionsPossible = count(arr, mid, n);
-            if (partitionsPossible > k) {
-                low = mid + 1;
+        int lo = 0;
+        int hi = sum;
+
+        int result = 0;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (possible(nums, k, mid)) {
+                result = mid;
+                hi = mid - 1;
             } else {
-                high = mid - 1;
+                lo = mid + 1;
             }
         }
-        return low;
+
+
+        return result;
     }
 
-    public int count(int[] arr, int sum, int n) {
-        int ct = 1;
-        long val = 0;
-        for (int i = 0; i < n; i++) {
-            if (val + arr[i] <= sum) {
-                val += arr[i];
+    public boolean possible(int[] nums, int k, int sum) {
+        // count the number of subarray such that the sum <= k;
+        int cnt = 1;
+        int currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > sum) return false;
+            if (currSum + nums[i] <= sum) {
+                currSum += nums[i];
             } else {
-                ct++;
-                val = arr[i];
+                cnt++;
+                currSum = nums[i];
             }
+        } 
+
+        if (cnt <= k) {
+            return true; 
+        } else {
+            return false;
         }
-        return ct;
     }
 }
+
+// Reframe this problem ->
+// int ans = x;
+// there must be k sub-arrays such that the the sum of subarrays must be lest than x;
