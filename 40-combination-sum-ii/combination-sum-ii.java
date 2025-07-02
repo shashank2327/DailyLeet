97a3abe7bb2ed6a37e898ae5cfc68a1b3bbbb522
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-02 17:06:39 +0530

    Time: 4 ms (40.32%) | Memory: 42.7 MB (99.84%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<List<Integer>> res = new ArrayList<>();
-        fun(candidates, 0, 0, target, new ArrayList<>(), res);
-        return res;
+        List<List<Integer>> result = new ArrayList<>();
+        fun(candidates, 0, 0, target, new ArrayList<>(), result);
+        return result;
     }
-
-    public void fun(int[] nums, int idx, int curr, int target, List<Integer> temp, List<List<Integer>> res) {
-        if (curr == target) {
-            res.add(new ArrayList<>(temp));
+    public void fun(int[] nums, int idx, int sum, int target, List<Integer> temp, List<List<Integer>> result) {
+        if (sum == target) {
+            result.add(new ArrayList<>(temp));
             return;
         }
-
-        if (curr > target || idx == nums.length) {
+        if (sum > target || idx == nums.length) {
             return;
         }
 
         for (int i = idx; i < nums.length; i++) {
-            if (i > idx && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
+            if (i > idx && nums[i] == nums[i - 1]) continue;
             temp.add(nums[i]);
-            fun(nums, i + 1, curr + nums[i], target, temp, res);
-
+            fun(nums, i + 1, sum + nums[i], target, temp, result);
             temp.remove(temp.size() - 1);
         }
     }
 }
+
+// -----> 10,1,2,7,6,1,5
+
+// -----> 1,1,2,5,6,7,10
