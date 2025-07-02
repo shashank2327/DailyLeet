5c2abae89cc27004995d0a55de77d1ac29a6ab98
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-02 18:15:56 +0530

    Time: 207 ms (21.44%) | Memory: 41.5 MB (77.25%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,48 +1,45 @@
 class Solution {
+
+    int[] dx = {0, 0, 1, -1};  // for horizontal movement;
+    int[] dy = {1, -1, 0, 0}; // for vertical movement;
+
     public boolean exist(char[][] board, String word) {
         int n = board.length;
         int m = board[0].length;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (dfs(board, i, j, n, m, word, 0)) {
+                if (dfs(board, word, 0, i, j, n, m)) {
                     return true;
                 }
             }
         }
-
         return false;
     }
 
-
-    public boolean dfs(char[][] board, int r, int c, int n, int m, String word,
-        int idx
-    ) 
-    {   
+    public boolean dfs(char[][] board, String word, int idx, int row, int col, int n, int m) {
         if (idx == word.length()) {
             return true;
-        }
-        if (r >= n || c >= m || r < 0 || c < 0) {
+        } 
+        if (row < 0 || col < 0 || row >= n || col >= m) {
             return false;
         }
+        if (word.charAt(idx) != board[row][col]) return false;
+        
+        char ch = board[row][col];
+        board[row][col] = '#';  // marked as visited;
 
-        if (word.charAt(idx) != board[r][c] || board[r][c] == ' ') {
-            return false;
+        for (int i = 0; i < 4; i++) {
+            int nr = dx[i] + row;
+            int nc = dy[i] + col;
+
+            if (dfs(board, word, idx + 1, nr, nc, n, m)) {
+                return true;
+            }
         }
 
-        char ch = board[r][c];
-        board[r][c] = ' '; // marking it as visited;
+        board[row][col] = ch;
 
-        boolean up = dfs(board, r - 1, c, n, m, word, idx + 1);
-        boolean down = dfs(board, r + 1, c, n, m, word, idx + 1);
-        boolean left = dfs(board, r, c - 1, n, m, word, idx + 1);
-        boolean right = dfs(board, r, c + 1, n, m, word, idx + 1);
-
-        if (up || down || left || right) {
-            return true;
-        }
-
-        board[r][c] = ch;
         return false;
     }
 }
