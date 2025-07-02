5df81d5f4a9494e339a2ba931a4d42c031382257
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-02 20:06:25 +0530

    Time: 32 ms (51.99%) | Memory: 59 MB (51.31%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,29 +1,31 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-
-        // Maintain of deque of decreasing values;
-
+        
         int n = nums.length;
         Deque<Integer> dq = new ArrayDeque<>();
-        int[] result = new int[n - k + 1];
-        int idx = 0;
-        for (int i = 0; i < nums.length; i++) {
 
-            // If the first element is not in the window size then remove it;
-            if (!dq.isEmpty() && dq.peekFirst() < i - k + 1) {
+        int[] res = new int[n - (k - 1)];
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // If the top value of my deque is out of the window then get it out of the dq;
+            while (!dq.isEmpty() && i - dq.peekFirst() >= k) {
                 dq.pollFirst();
             }
 
-            while(!dq.isEmpty() && nums[i] > nums[dq.peekLast()]) {
+            while (!dq.isEmpty() && nums[i] > nums[dq.peekLast()]) {
                 dq.pollLast();
             }
             dq.addLast(i);
+
             if (i >= k - 1) {
-                result[idx] = nums[dq.peekFirst()];
-                idx++;
+                res[idx++] = nums[dq.peekFirst()];
             }
         }
 
-        return result;
+        return res;
     }
 }
+
+// i - dq.peekFirst() > k + 1;
