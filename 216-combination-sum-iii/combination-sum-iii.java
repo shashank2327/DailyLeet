e32085d0f4dff541bdbd406f6c0b6b03222a1055
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-02 17:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (42.67%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,32 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        //[1, 2, 3, 4, 5, 6, 7, 8, 9];
-        List<List<Integer>> res = new ArrayList<>();
-        fun(1, 0, k, 0, n, new ArrayList<>(), res);
-        return res;
+        List<List<Integer>> result = new ArrayList<>();
+        fun(1, k, 0, n, new ArrayList<>(), result);
+        return result;
     }
     public void fun(
         int idx,
-        int cnt,
         int k,
-        int currSum,
-        int target,
+        int sum,
+        int n,
         List<Integer> temp,
-        List<List<Integer>> res
-    ) 
-    {   
-        if (cnt > k || currSum > target) {
+        List<List<Integer>> result
+    ) {
+        if (k == 0) {
+            if (sum == n) {
+                result.add(new ArrayList<>(temp));
+            }
             return;
         }
-
-        if (cnt == k && currSum == target) {
-            res.add(new ArrayList<>(temp));
-            return;
-        }
+        if (sum > n) return;
 
         for (int i = idx; i <= 9; i++) {
             temp.add(i);
-            fun(i + 1, cnt + 1, k, currSum + i, target, temp, res);
+            fun(i + 1, k - 1, sum + i, n, temp, result);
             temp.remove(temp.size() - 1);
         }
     }
