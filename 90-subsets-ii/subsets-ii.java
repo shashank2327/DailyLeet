ce66b7e247a45050e4594fc2d7c8a301e6f34643
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-02 17:12:14 +0530

    Time: 2 ms (30.54%) | Memory: 43.6 MB (61.04%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -5,13 +5,14 @@ class Solution {
         fun(nums, 0, new ArrayList<>(), result);
         return result;
     }
-    public void fun(int[] nums, int idx, List<Integer> temp, List<List<Integer>> res) {
-        res.add(new ArrayList<>(temp));
+    public void fun(int[] nums, int idx, List<Integer> temp, List<List<Integer>> result) {
+        result.add(new ArrayList<>(temp));
 
         for (int i = idx; i < nums.length; i++) {
             if (i > idx && nums[i] == nums[i - 1]) continue;
+            
             temp.add(nums[i]);
-            fun(nums, i + 1,  temp, res);
+            fun(nums, i + 1, temp, result);
             temp.remove(temp.size() - 1);
         }
     }
