2eb66df56526b10cdee72824db8169a79c3a2db7
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-03 11:58:56 +0530

    Time: 12 ms (80.00%) | Memory: 45 MB (72.31%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
@@ -1,50 +1,50 @@
 class Tuple {
-    int hth;
+    int health;
     int row;
     int col;
 
-    public Tuple (int hth, int row, int col) {
-        this.hth = hth;
+    public Tuple(int health, int row, int col) {
+        this.health = health;
         this.row = row;
         this.col = col;
     }
 }
-
 class Solution {
+
+    // for going in four directions;
+    int[] delRow = {1, -1, 0, 0};
+    int[] delCol = {0, 0, 1, -1};
+
     public boolean findSafeWalk(List<List<Integer>> grid, int health) {
         int n = grid.size();
         int m = grid.get(0).size();
 
-        int[][] heal = new int[n][m]; // Keep track of maximum health at each point;
-        heal[0][0] = health - grid.get(0).get(0);
-        Queue<Tuple> q = new LinkedList<>();
-        q.add(new Tuple(heal[0][0], 0, 0));
+        // keep tracks of maximum health for each row and col;
+        int[][] healthTracker = new int[n][m];
+        PriorityQueue<Tuple> maxHeap = new PriorityQueue<>((t1, t2) -> t2.health - t1.health);
+        healthTracker[0][0] = health - grid.get(0).get(0);  
+        maxHeap.offer(new Tuple(healthTracker[0][0], 0, 0));
 
-        while (!q.isEmpty()) {
-            int h = q.peek().hth;
-            int r = q.peek().row;
-            int c = q.peek().col;
-            q.remove();
 
-            if (h < heal[r][c]) continue;
-
-            int[] dx = {0, 0, 1, -1};
-            int[] dy = {1, -1, 0, 0};
+        while (!maxHeap.isEmpty()) {
+            Tuple token = maxHeap.poll();
+            int healthRemain = token.health;
+            int row = token.row;
+            int col = token.col;
 
             for (int i = 0; i < 4; i++) {
-                int nr = r + dx[i];
-                int nc = c + dy[i];
+                int nr = row + delRow[i];
+                int nc = col + delCol[i];
 
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                    int newHealth = h - grid.get(nr).get(nc);
-                    if (newHealth > heal[nr][nc]) {
-                        heal[nr][nc] = newHealth;
-                        q.add(new Tuple(newHealth, nr, nc));
+                if (nr >= 0 && nc >= 0 && nr < n && nc < m) {
+                    if (healthRemain - grid.get(nr).get(nc) > healthTracker[nr][nc]) {
+                        healthTracker[nr][nc] = healthRemain - grid.get(nr).get(nc);
+                        maxHeap.offer(new Tuple(healthTracker[nr][nc], nr, nc));
                     }
                 }
             }
         }
 
-        return heal[n - 1][m - 1] <= 0 ? false : true;
+        return healthTracker[n - 1][m - 1] >= 1;
     }
 }
