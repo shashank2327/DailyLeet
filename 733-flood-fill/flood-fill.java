70513b295eeaa2c083f21163239b4f234c3d422d
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-03 07:29:31 +0530

    Time: 1 ms (47.55%) | Memory: 45 MB (50.65%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,42 +1,25 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int iniColor = image[sr][sc];
         int n = image.length;
         int m = image[0].length;
-        int[][] ans = new int[n][m];
-        for (int i  = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                ans[i][j] = image[i][j];
-            }
-        }
+        boolean[][] vis = new boolean[n][m];
+        dfs(image, vis, sr, sc, image[sr][sc], color);
 
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
-        dfs(sr, sc, ans, image, color, dx, dy, iniColor);
-        return ans;
+        return image;
     }
+    int[] dx = {0, 0, 1, -1};
+    int[] dy = {1, -1, 0, 0};
 
-    private void dfs
-    (
-        int row, 
-        int col, 
-        int[][] ans, 
-        int[][] image, 
-        int color,
-        int[] dx, 
-        int[] dy,
-        int iniColor
-    ) {
-        ans[row][col] = color;
-        int n = image.length;
-        int m = image[0].length;
+    public void dfs(int[][] image, boolean[][] vis, int sr, int sc, int startColor, int color) {
+        vis[sr][sc] = true;
+        image[sr][sc] = color;
         for (int i = 0; i < 4; i++) {
-            int nrow = row + dx[i];
-            int ncol = col + dy[i];
-            if (nrow >= 0 && ncol >= 0 && ncol < m && nrow < n &&
-                image[nrow][ncol] == iniColor && ans[nrow][ncol] != color
+            int nr = sr + dx[i];
+            int nc = sc + dy[i];
+            if (nr >= 0 && nc >= 0 && nr < image.length && nc < image[0].length && !vis[nr][nc]
+                && image[nr][nc] == startColor
             ) {
-                dfs(nrow, ncol, ans, image, color, dx, dy, iniColor);
+                dfs(image, vis, nr, nc, startColor, color);
             }
         }
     }
