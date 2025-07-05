d2d4e92043129a15e33bb69288f55868217c1990
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-05 08:34:23 +0530

    Time: 6 ms (54.69%) | Memory: 45.5 MB (60.19%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,53 +1,40 @@
 class Solution {
-
-    private Map<String, PriorityQueue<String>> buildGraph(List<List<String>> tickets) {
-        Map<String, PriorityQueue<String>> graph = new HashMap<>();
-
-        for (List<String> ticket : tickets) {
-            String src = ticket.get(0); 
-            String dst = ticket.get(1);
-            if (!graph.containsKey(src)) {
-                graph.put(src, new PriorityQueue<>());
-            }
-            graph.get(src).add(dst);
-        }
-
-        return graph;
-    }
-
-    private void dfs(
-        List<String> res,
-        Map<String, PriorityQueue<String>> graph,
-        String src
-    ) {
-
-        //base case;
-        //there is no dst from the given src
-        if (graph.get(src) == null || graph.get(src).isEmpty()) {
-            res.add(src);
-            return;
-        }
-
-        while(!graph.get(src).isEmpty()) {
-            String nxtDst = graph.get(src).poll();
-            dfs(res, graph, nxtDst);
-        }
-
-        res.add(src);
-    }
-
     public List<String> findItinerary(List<List<String>> tickets) {
         
+        // edge_case1 -> If there are no tickets;
         if (tickets == null || tickets.isEmpty()) {
             return Collections.EMPTY_LIST;
         }
 
-        Map<String, PriorityQueue<String>> graph = buildGraph(tickets);
 
-        List<String> res = new ArrayList<>();
-        dfs(res, graph, "JFK");
+        // building my graph;
+        Map<String, PriorityQueue<String>> graph = new HashMap<>();
+        for (List<String> ticket : tickets) {
+            String src = ticket.get(0);
+            String dst = ticket.get(1);
 
-        Collections.reverse(res);
-        return res;
+            graph.putIfAbsent(src, new PriorityQueue<>());
+            graph.get(src).add(dst);
+        }
+
+        List<String> result = new ArrayList<>();
+        dfs(graph, "JFK", result);
+
+        Collections.reverse(result);
+        return result;
+    }
+
+    private void dfs(Map<String, PriorityQueue<String>> graph, String src, List<String> result) {
+        
+        if (graph.get(src) == null || graph.get(src).isEmpty()) {
+            result.add(src);
+            return;
+        }
+        while (!graph.get(src).isEmpty()) {
+            String dst = graph.get(src).poll();
+            dfs(graph, dst, result);
+        }
+
+        result.add(src);
     }
 }
