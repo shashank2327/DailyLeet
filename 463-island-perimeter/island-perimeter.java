2eba1e0d307dc6312d1358e48b981cad3ce8634b
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-05 16:25:57 +0530

    Time: 7 ms (26.57%) | Memory: 45.4 MB (90.50%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,54 +1,36 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        int n = grid.length;  // number of rows;
+        int m = grid[0].length;  // number of cols;.
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
         int cnt = 0;
+
         for (int i = 0; i < n; i++) {
-            int left = -1;
-            int right = 0;
-            while (right < m) {
-                if (left == -1) {
-                    if (grid[i][right] == 1) {
-                        cnt++;
-                    }
-                } else {
-                    if (grid[i][left] == 0 && grid[i][right] == 1) {
-                        cnt++;
-                    } else if (grid[i][left] == 1 && grid[i][right] == 0) {
-                        cnt++;
-                    }
-                }
-                left++;
-                right++;
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    for (int k = 0; k < 4; k++) {
+                        int nr = i + dx[k];
+                        int nc = j + dy[k];
+                        if (!isValid(grid, n, m, nr, nc)) {
+                            cnt++;
+                        }
+                    } 
+                }         
             }
-
-            if (left == m - 1 && grid[i][left] == 1) cnt++; 
-        }
-
-
-        for (int j = 0; j < m; j++) {
-            int left = -1;
-            int right = 0;
-            while (right < n) {
-                if (left == -1) {
-                    if (grid[right][j] == 1) {
-                        cnt++;
-                    }
-                } else {
-                    if (grid[left][j] == 0 && grid[right][j] == 1) {
-                        cnt++;
-                    } else if (grid[left][j] == 1 && grid[right][j] == 0) {
-                        cnt++;
-                    }
-                }
-                left++;
-                right++;
-            }
-
-            if (left == n - 1 && grid[left][j] == 1) cnt++; 
         }
 
         return cnt;
+
+    }
+
+    boolean isValid(int[][] grid, int n, int m, int row, int col) {
+        if (row >= 0 && col >= 0 && row < n && col < m && grid[row][col] == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
@@ -58,3 +40,4 @@ class Solution {
     0 1 0 0
     1 1 0 0
 */
+
