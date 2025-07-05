2129c882dea9abcae29a6f968ee7e779a1f45473
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-05 11:20:58 +0530

    Time: 2 ms (69.64%) | Memory: 44.5 MB (70.64%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,50 +1,21 @@
 class Solution {
-    int[] dx = {1, -1, 0, 0};
-    int[] dy = {0, 0, 1, -1};
     public int maxAreaOfIsland(int[][] grid) {
-        int n = grid.length; // number of rows;
-        int m = grid[0].length; // number of cols;
-
-        boolean[][] vis = new boolean[n][m]; // keep tracks of cell, being visited;
-        int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && !vis[i][j]) {
-                    int currArea = bfs(grid, i, j, n, m, vis);
-
-                    maxArea = Math.max(maxArea, currArea);
-                }
+        int area = 0;
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[0].length; ++j) {
+                if (grid[i][j] == 1)
+                    area = Math.max(area, dfs(grid, i, j));
             }
         }
 
-        return maxArea;
+        return area;
+        
     }
 
-    public int bfs(int[][] grid, int r, int c, int n, int m, boolean[][] vis) {
-        int cnt = 0;
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.offer(new int[]{r, c});
-        cnt++;
-        vis[r][c] = true;
-
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            int row = cell[0];
-            int col = cell[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + dx[i];
-                int ncol = col + dy[i];
-
-                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
-                    cnt++;
-                    q.offer(new int[]{nrow, ncol});
-                    vis[nrow][ncol] = true;
-                }
-            }
-        }
-
-        return cnt;
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1)
+            return 0;
+        grid[i][j] = 0;
+        return 1 + dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1);
     }
 }
