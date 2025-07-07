2103dd41d7bcaab814da1e77ba940a1dd12d99ae
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-07 08:00:36 +0530

    Time: 1 ms (100.00%) | Memory: 47.4 MB (84.43%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,23 +1,32 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        return atLeast(nums, goal) - atLeast(nums, goal - 1);
+    public int numSubarraysWithSum(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
 
-    public int atLeast(int[] nums, int k) {
+    public int atMost(int[] nums, int k) {
         int n = nums.length;
-        int l = 0;
-        int r = 0;
-        int count = 0;
-        int sum = 0;
-        while (r < n) {
-            sum += nums[r];
-            while (sum > k && l <= r) {
-                sum -= nums[l];
-                l++;
+        int count = 0; // count of the subarray;
+        int sum = 0; // keeps track of current sum;
+
+        // pointers
+        int left = 0;
+        int right = 0;
+
+        while (right < n) {
+            sum += nums[right];
+            while (sum > k && left <= right) {
+                sum -= nums[left];
+                left++;
             }
-            count += r - l + 1;
-            r++;
+
+            count += right - left + 1;
+            right++;
         }
         return count;
     }
 }
+
+// sliding window is usefull when the required event increases or decreases in a particular directions
+
+/*
+*/
