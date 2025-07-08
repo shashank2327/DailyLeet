fd45384b9cf32d93f547ac857ec96ce6968159c7
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-08 11:10:07 +0530

    Time: 323 ms (16.34%) | Memory: 75.9 MB (21.07%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,57 +1,30 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-/*-------------------------------------------------------------------------------------------*/        
-        int[][] memo = new int[n + 1][n];
-        for (int[] x : memo) Arrays.fill(x, -1);
-        // return f(nums, -1, 0, memo);
-/*-------------------------------------------------------------------------------------------*/  
-
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int i = 1;
-        int max = 0;
-        int[] hash = new int[n];
-        for (int k = 0; k < n; k++) hash[k] = k;
-        while (i != n) {
-            int j = 0;
-            while (j < i) {
-                if (nums[j] < nums[i] && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    hash[i] = j;
-                }
-                j++;
-            }
-            i++;
+        int[][] memo = new int[n + 1][n + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
         }
-
-        // Printing LIS
-        int hashIndex = -1;
-        for (int k = 0; k < n; k++) {
-            if (dp[k] > max) {
-                max = dp[k];
-                hashIndex = k;
-            }
-        }
-
-        int[] printLIS = new int[max];
-        for (int k = max - 1; k >= 0; k--) {
-            printLIS[k] = nums[hashIndex];
-            hashIndex = hash[hashIndex];
-        }
-        System.out.println(Arrays.toString(printLIS));
-        return max;
+        return solve(nums, -1, 0, memo);
     }
 
-/*--------------------------memo + rec-------------------------------------------------------*/
-    private int f(int[] nums, int i, int j, int[][] memo) {
-        if (j == nums.length) return 0;
-        if (memo[i + 1][j] != -1) return memo[i + 1][j];
-        int take = 0;
-        if (i == -1 || nums[j] > nums[i]) {
-            take = 1 + f(nums, j, j + 1, memo);
+    public int solve(int[] nums, int prevIdx, int currIdx, int[][] memo) {
+        if (currIdx == nums.length) {
+            return 0;
         }
-        int notTake = f(nums, i, j + 1,memo);
-        return memo[i + 1][j] = Math.max(take, notTake);
+        if (memo[prevIdx + 1][currIdx + 1] != -1) {
+            return memo[prevIdx + 1][currIdx + 1];
+        }
+        int ans = 0;
+        // take it
+        if (prevIdx == -1 || nums[prevIdx] < nums[currIdx]) {
+            ans = Math.max(ans, 1 + solve(nums, currIdx, currIdx + 1, memo));
+        }
+
+        // do not take it;
+        ans = Math.max(ans, solve(nums, prevIdx, currIdx + 1, memo));
+
+
+        return memo[prevIdx + 1][currIdx + 1] = ans;
     }
 }
