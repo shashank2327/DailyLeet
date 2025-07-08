af3b9ba461e7bbb753ef92797aa8d011490f8389
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-08 08:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (50.83%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,41 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        // int[] memo = new int[n + 1];
-        // Arrays.fill(memo, -1);
-        // return memoFunction(n, memo);
-        return dp(n);
-    }
-
-    // Every time I have two options
-    // Either to jump one step or to jump 2 step;
-    // if numbers of steps i completed through jumping is > n then return 0 if = then retur 1;
-
-    private int recursiveFunction(int n) {
-        if (n == 0) return 1;
-        if (n < 0) return 0;
-
-        int ways = recursiveFunction(n - 1) + recursiveFunction(n - 2);
-        return ways;
-    }
-
-
-    private int memoFunction(int n, int[] memo) {
-        if (n == 0) return 1;
-        if (n < 0) return 0;
-        if (memo[n] != -1) return memo[n];
-        int ways = memoFunction(n - 1, memo) + memoFunction(n - 2, memo);
-        memo[n] = ways;
-        return memo[n];
-    }
-
-    private int dp(int n) {
-        int a = 0;
-        int b = 1;
+        int prev2 = 0;
+        int prev1 = 1;
+        int curr = 0;
         for (int i = 1; i <= n; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return b;
+
+        return curr;
     }
 }
