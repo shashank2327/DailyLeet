a2c8a4ed4edf7b6480c5995b0c2180053c71a612
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-09 08:59:10 +0530

    Time: 10 ms (9.88%) | Memory: 56.4 MB (6.52%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int[][][] memo = new int[s1.length() + 1][s2.length() + 1][s3.length() + 1];
+        for (int[][] mat : memo) {
+            for (int[] row : mat) {
+                Arrays.fill(row, -1);
+            }
+        }
+        return solve(s1, s2, s3, 0, 0, 0, memo);
+    }
+    public boolean solve(String s1, String s2, String s3, int i, int j , int k, int[][][] memo) {
+        if (k == s3.length()) {
+            if (i != s1.length() || j != s2.length()) {
+                return false;
+            }
+            return true;
+        }
+        if (i < s1.length() && j < s2.length() && s1.charAt(i) != s3.charAt(k) && s2.charAt(j) != s3.charAt(k)) {
+            return false;
+        }
+
+        if (memo[i][j][k] != -1) {
+            return memo[i][j][k] == 1 ? true : false;
+        }
+
+        boolean result = false;
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+            result = result || solve(s1, s2, s3, i + 1, j, k + 1, memo);
+        }
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            result = result || solve(s1, s2, s3, i, j + 1, k + 1, memo);
+        }
+
+
+        memo[i][j][k] = result == true ? 1 : 0;
+        return result;
+    }
+}
+
+// |n - m| <= 1 is only to confuse; there is no use of that
