7a9d091c95c638f2f2323774774a3c5413998109
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-09 07:38:07 +0530

    Time: 630 ms (5.04%) | Memory: 126.9 MB (5.04%) - LeetSync

---
+++ 3908-minimum-time-for-k-connected-components/minimum-time-for-k-connected-components.java
@@ -0,0 +1,74 @@
+class DisjointSet {
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            parent.add(i);
+            size.add(i);
+        }
+    } 
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) {
+            return;
+        }
+
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_u) + size.get(ulp_v));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_v) + size.get(ulp_u));
+        }
+    }
+}
+class Solution {
+    public int minTime(int n, int[][] edges, int k) {
+        int m = edges.length;
+        int lo = 0;
+        int hi = (int) 1e9;
+        int result = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            DisjointSet ds = new DisjointSet(n);
+            for (int[] edge : edges) {
+                int u = edge[0];
+                int v = edge[1];
+                int time = edge[2];
+
+                // means the edge can not be removed;
+                if (time > mid) {
+                    ds.unionBySize(u, v);
+                }
+            }
+
+            Set<Integer> set = new HashSet<>();
+            for (int i = 0; i < n; i++) {
+                set.add(ds.findUPar(i));
+            }
+
+            if (set.size() >= k) {
+                result = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+
+        return result;
+    }
+}
