4443de297ac4c6a57872a51db35e0dbb882932c1
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-09 09:19:52 +0530

    Time: 22 ms (22.08%) | Memory: 51.5 MB (21.95%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -3,42 +3,37 @@ class Solution {
         int n = s.length();
         int m = t.length();
 
-/*-------------------------------------------------------------------------*/
-        int[][] memo = new int[n][m];
-        for (int[] x : memo) {
-            Arrays.fill(x, -1);
+        int[][] memo = new int[n + 1][m + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
         }
-        // return f(s, t, s.length() - 1, t.length() - 1, memo);
-
-/*-------------------------------------------------------------------------*/
-
-        // Tabulation Code --> Dervied from recursive code;
-
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-        return dp[n][m];
+        return solve(s, t, 0, 0, memo);
     }
 
-
-/*--------------------------recursive + memo-------------------------------------------------*/
-    private int f(String s, String t, int i, int j, int[][] memo) {
-        if (j < 0) return 1;
-        if (i < 0) return 0;
-        if (memo[i][j] != -1) return memo[i][j];
-        if (s.charAt(i) == t.charAt(j)) {
-            return memo[i][j] = f(s, t, i - 1, j - 1, memo) + f(s, t, i - 1, j, memo);
+    public int solve(String s, String t, int i, int j, int[][] memo) {
+        if (j == t.length()) {
+            return 1;
+        }
+        if (i == s.length()) {
+            return 0;
         }
 
-        return memo[i][j] = f(s, t, i - 1, j, memo);
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        int result = 0;
+
+        // if matches;
+        // I have two options. Either take it or do not take it;
+        if (s.charAt(i) == t.charAt(j)) {
+            // take;
+            result += solve(s, t, i + 1, j + 1, memo);
+            // not take;
+            result += solve(s, t, i + 1, j, memo);
+        } else {
+            result += solve(s, t, i + 1, j, memo);
+        }
+
+        return memo[i][j] = result;
     }
 }
