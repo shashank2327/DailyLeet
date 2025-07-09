b4e8861476b27b3beb47c1069a5fc9d670b93e62
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-09 09:33:55 +0530

    Time: 10 ms (60.27%) | Memory: 45.3 MB (15.67%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,21 +1,31 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(nums, 0, 0, target);
+        int n = nums.length;
+        int[][] memo = new int[n + 1][2002];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return solve(nums, 0, 0, target, memo);
     }
 
-    public int solve(int[] nums, int sum, int idx, int target) {
+    public int solve(int[] nums, int sum, int idx, int target, int[][] memo) {
         if (idx == nums.length) {
             if (sum == target) {
                 return 1;
             }
             return 0;
         }
+        if (sum < -1000 || sum > 1000) return 0;
+
+        if (memo[idx][sum + 1000] != -1) {
+            return memo[idx][sum + 1000];
+        }
 
         int result = 0;
 
-        result += solve(nums, sum + nums[idx] , idx + 1, target);
-        result += solve(nums, sum - nums[idx], idx + 1, target);
+        result += solve(nums, sum + nums[idx], idx + 1, target, memo);
+        result += solve(nums, sum - nums[idx], idx + 1, target, memo);
 
-        return result;
+        return memo[idx][sum + 1000] = result;
     }
 }
