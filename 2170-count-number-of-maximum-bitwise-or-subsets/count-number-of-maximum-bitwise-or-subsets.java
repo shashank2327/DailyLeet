465d320352d1afe266fb3c92d11c8bf87522925e
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-07-28 17:01:08 +0530

    Time: 11 ms (34.12%) | Memory: 61.7 MB (6.40%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,25 +1,29 @@
 class Solution {
     public int countMaxOrSubsets(int[] nums) {
+        int n = nums.length;
         int max = 0;
         for (int num : nums) {
             max = max | num;
         }
 
-        return countSubsets(nums, 0, 0, max);
+        Integer[][] memo = new Integer[n][max + 1];
+        return countSubsets(nums, 0, 0, max, memo);
     }
 
-    public int countSubsets(int[] nums, int idx, int currOr, int targetOr) {
+    public int countSubsets(int[] nums, int idx, int currOr, int targetOr, Integer[][] memo) {
         if (idx == nums.length) {
             return (currOr == targetOr) ? 1 : 0;
         }
 
+        if (memo[idx][currOr] != null) return memo[idx][currOr];
+
         // take it
-        int take = countSubsets(nums, idx + 1, currOr | nums[idx], targetOr);
+        int take = countSubsets(nums, idx + 1, currOr | nums[idx], targetOr, memo);
 
         // do not take it
-        int notTake = countSubsets(nums, idx + 1, currOr, targetOr);
+        int notTake = countSubsets(nums, idx + 1, currOr, targetOr, memo);
 
 
-        return take + notTake;
+        return memo[idx][currOr] = take + notTake;
     }
 }
