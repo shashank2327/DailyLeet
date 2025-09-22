d60a9aa089ea47d497a64eb416dfc760a8d3b23b
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-09-22 19:22:22 +0530

    Time: 1 ms (99.44%) | Memory: 42.2 MB (86.43%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
         int[] freq = new int[101];
-
+        int max = 0;
+        int cnt = 0;
         for (int num : nums) {
             freq[num]++;
+            if (freq[num] > max) {
+                cnt = 1;
+                max = freq[num];
+            } else if (freq[num] == max) {
+                cnt++;
+            }
         }
 
-        Arrays.sort(freq);
-
-        int val = freq[100];
-        int cnt = 0;
-        for (int i = 0; i <= 100; i++) {
-            if (freq[i] == val) cnt++;
-        }
-
-        return cnt * val;
+        return cnt * max;
     }
 }
