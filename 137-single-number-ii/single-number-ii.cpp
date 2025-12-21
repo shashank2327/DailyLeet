d97b1f318ddb1c16b5bd8798b18a2bb29f251a37
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-12-21 20:00:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (37.39%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,15 +2,32 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int res = 0;
+
+        // we are going to every 32 positions;
         for (int i = 0; i <= 31; i++) {
             int sum = 0;
             for (int num : nums) {
-                sum += (num >> i) & 1;
+                sum += (num >> i) & 1;  
             }
-            sum = sum % 3;
-            res = res | (sum << i);
+
+            sum = sum % 3;  // the sum value will either come 0 or 1;
+            res = res | (sum << i);  // we setting the ith bit;
         }
 
         return res;
     }
 };
+
+/*
+    [6, 6, 6, 3, 3, 3, 4]  = 4
+
+    1 1 0
+    1 1 0
+    1 1 0
+    0 1 1
+    0 1 1
+    0 1 1
+    1 0 0
+    4 6 3
+    1 0 0  = 4 
+*/
