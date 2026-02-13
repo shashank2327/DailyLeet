95905004e47b26241ddbcfef7dab2c817dc2fff8
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2026-02-14 00:02:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (32.88%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,35 @@
 class Solution {
     public int climbStairs(int n) {
-        int prev2 = 0;
-        int prev1 = 1;
-        int curr = 0;
-        for (int i = 1; i <= n; i++) {
-            curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+        if (n <= 2) return n;
+        int[] dp = new int[n + 1]; // each index represent a number of ways to reach that number of steps
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            // number i
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        return curr;
+        return dp[n];
+        // return solve(n);
+    }   
+
+    // What is the base case
+    // What if there is one step : 1 way
+    // whay if there is 2 steps : 2 ways
+    public int solve(int n) {
+
+        if (n <= 0) return 0;
+
+        if (n == 1 || n == 2) {
+            return n;
+        }
+
+        // to reach the nth step:
+        // either i will come from (n - 1)th step
+        // or i will come from (n-2)th step;
+
+        return solve(n - 1) + solve(n - 2);
     }
 }
