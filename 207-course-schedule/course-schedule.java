e3395690f61213b5e2e253cd66fd23f7a88ec8e6
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2026-08-16 20:35:55 +0530

    Time: 7 ms (56.09%) | Memory: 47.1 MB (65.11%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,54 +1,40 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
+
         for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
-        }
+        } 
 
-        int n = prerequisites.length;
+        int[] indeg = new int[numCourses];
 
-        for (int i = 0; i < n; i++) {
-            int b = prerequisites[i][1];
-            int a = prerequisites[i][0];
-            adj.get(b).add(a);
-        }
+        for (int[] edge: prerequisites) {
+            int u = edge[0];
+            int v = edge[1];
 
-
-        int[] indegree = new int[numCourses];
-
-        for (int i = 0; i < numCourses; i++) {
-            for (int it : adj.get(i)) {
-                indegree[it]++;
-            }
+            adj.get(v).add(u);
+            indeg[u]++;
         }
 
         Queue<Integer> q = new LinkedList<>();
 
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-            }
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) q.add(i);
         }
 
-        int cnt = 0;
-        
+
         while (!q.isEmpty()) {
-            int node = q.poll();
-            cnt++;
-
-            for (int it : adj.get(node)) {
-                indegree[it]--;
-                if (indegree[it] == 0) {
-                    q.add(it);
-                }
+            int nd = q.poll();
+            for (int nbr: adj.get(nd)) {
+                indeg[nbr]--;
+                if (indeg[nbr] == 0) q.add(nbr);
             }
         }
 
-        return cnt == numCourses;
+        for (int deg: indeg) {
+            if (deg > 0) return false;
+        }
+
+        return true;
     }
 }
-
-// [a, b]
-// b ----> a // b should come first then a should come.
-// I will not be able to take course if there is cycle present.
