0b999f874cc473e069b3e0b68e826acb80c6c09b
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2026-08-18 18:02:14 +0530

    Time: 32 ms (91.35%) | Memory: 54.7 MB (96.17%) - LeetSync

--- 630-course-schedule-iii/course-schedule-iii.java
+++ 630-course-schedule-iii/course-schedule-iii.java
@@ -1,30 +1,23 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        int n = courses.length;
         Arrays.sort(courses, (a, b) -> a[1] - b[1]);
-        List<Integer> ls = new ArrayList<>();
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         int time = 0;
-        for (int[] c : courses) {
-            if (time + c[0] <= c[1]) {
-                ls.add(c[0]);
-                time += c[0];
+        for (int[] course: courses) {
+            int dur = course[0];
+            int end = course[1];
+            if (time + dur <= end) {
+                time += dur;
+                pq.offer(dur);
             } else {
-                int max_i = 0;
-                for (int i = 1; i < ls.size(); i++) {
-                    if (ls.get(i) > ls.get(max_i)) {
-                        max_i = i;
-                    }
-                }
-
-                if (ls.size() > 0 && ls.get(max_i) > c[0]) {
-                    time += c[0] - ls.get(max_i);
-                    ls.set(max_i, c[0]);
+                if (!pq.isEmpty() && pq.peek() > dur) {
+                    time += dur - pq.poll();
+                    pq.offer(dur);
                 }
             }
         }
 
-        return ls.size();
+        return pq.size();
     }
 }
 
