186d32e816bc9e75e298706e119311f770d86991
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2026-08-21 20:55:37 +0530

    Time: 14 ms (36.72%) | Memory: 51.8 MB (77.84%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,49 +1,54 @@
 class Solution {
-    private static final int MOD = (int)1e9 + 7;
+
+    private static final int MOD = (int) 1e9 + 7;
+
     public int countPaths(int n, int[][] roads) {
-        
-        List<List<int[]>> graph = new ArrayList<>();
+
+        List<List<int[]>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
 
-        for (int[] road : roads) {
+        for (int[] road: roads) {
             int u = road[0];
             int v = road[1];
-            int timeTaken = road[2];
+            int time = road[2];
 
-            graph.get(u).add(new int[] {v, timeTaken});
-            graph.get(v).add(new int[] {u, timeTaken});
+            adj.get(u).add(new int[] {v, time});
+            adj.get(v).add(new int[] {u, time});
         }
-
 
         long[] time = new long[n];
         Arrays.fill(time, Long.MAX_VALUE);
 
         int[] ways = new int[n];
 
-
-        // {node, timeTakenToReach}
-        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));
+        // {node, timeTakenSoFar}
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[1], b[1]));
         time[0] = 0;
         ways[0] = 1;
+
         pq.offer(new long[]{0, 0});
-        
+
         while (!pq.isEmpty()) {
             long[] token = pq.poll();
-            long timeTaken = token[0];
-            int node = (int)token[1];
+            int nd = (int)token[0];
+            long timeTaken = token[1];
 
-            for (int[] adj : graph.get(node)) {
-                int adjNode = adj[0];
-                int tm = adj[1];
+            if (timeTaken > time[nd]) continue;
 
-                if (tm + timeTaken < time[adjNode]) {
-                    time[adjNode] = tm + timeTaken;
-                    ways[adjNode] = (ways[node]) % MOD;
-                    pq.offer(new long[]{time[adjNode], adjNode});
-                } else if (tm + timeTaken == time[adjNode]) {
-                    ways[adjNode] = (ways[adjNode] + ways[node]) % MOD;
+            for (int[] edge: adj.get(nd)) {
+                int nbr = edge[0];
+                int tm = edge[1];
+
+                long newTime = tm + timeTaken;
+
+                if (newTime < time[nbr]) {
+                    time[nbr] = newTime;
+                    ways[nbr] = ways[nd] % MOD;
+                    pq.offer(new long[]{nbr, time[nbr]});
+                } else if (newTime == time[nbr]) {
+                    ways[nbr] = (ways[nbr] + ways[nd]) % MOD;
                 }
             }
         }
