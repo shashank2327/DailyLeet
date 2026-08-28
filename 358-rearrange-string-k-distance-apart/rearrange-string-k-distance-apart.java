317ad03db4106e432fb8b83faaaa5c75e23c1887
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2026-08-29 01:57:54 +0530

    Time: 288 ms (5.88%) | Memory: 47.3 MB (46.08%) - LeetSync

--- 358-rearrange-string-k-distance-apart/rearrange-string-k-distance-apart.java
+++ 358-rearrange-string-k-distance-apart/rearrange-string-k-distance-apart.java
@@ -67,3 +67,45 @@ class Solution {
         return sb.toString();
     }
 }
+
+/*
+
+class Solution {
+    public String rearrangeString(String s, int k) {
+        // build frequencies mapping
+        int[] frequencies = new int[26];
+        for (char c : s.toCharArray()) {
+            frequencies[c - 'a']++;
+        }
+
+        // create max heap of [character, its frequency]
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        for (int i = 0; i < 26; ++i) {
+            if (frequencies[i] != 0) maxHeap.add(new int[]{i, frequencies[i]});
+        }
+
+        // cooling queue [char, it frequency]
+        Queue<int[]> coolingQueue = new ArrayDeque<>();
+
+        StringBuilder res = new StringBuilder();
+        while (!maxHeap.isEmpty()) {
+            int[] cur = maxHeap.poll();
+
+            // append to result and add to cooling queue (even if freq = 0)
+            res.append((char) (cur[0] + 'a'));
+            cur[1]--;
+
+            coolingQueue.add(cur);
+
+            // add back to heap after k positions passed
+            if (coolingQueue.size() >= k) {
+                int[] release = coolingQueue.poll();
+                if (release[1] > 0) maxHeap.add(release);
+            }
+        }
+
+        return res.length() < s.length() ? "" : res.toString();
+    }
+}
+
+*/
