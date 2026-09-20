b1b97650989eda3fcfec2b4764beb9d93af7fe0f
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2026-09-20 20:35:07 +0530

    Time: 22 ms (46.05%) | Memory: 54.1 MB (21.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,13 +3,9 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
 
- /*-----------------------------------------------------------------------------------------------------------*/       
-        int[][] memo = new int[n][m];
-        for (int[] x : memo) Arrays.fill(x, -1);
-        // return f(text1, text2, n - 1, m - 1, memo);
-/*------------------------------------------------------------------------------------------------------------*/
 
         int[][] dp = new int[n + 1][m + 1];
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
@@ -19,19 +15,7 @@ class Solution {
                 }
             }
         }
+
         return dp[n][m];
-    }
-
-    // Recursive + memo
-    private int f(String s1, String s2, int i, int j, int[][] memo) {
-        if (i < 0 || j < 0) return 0;
-        if (memo[i][j] != -1) return memo[i][j];
-        if (s1.charAt(i) == s2.charAt(j)) {
-            memo[i][j] = 1 + f(s1, s2, i - 1, j - 1, memo);
-            return memo[i][j];
-        }
-
-        memo[i][j] = Math.max(f(s1, s2, i - 1, j, memo), f(s1, s2, i, j - 1, memo));
-        return memo[i][j];
     }
 }
